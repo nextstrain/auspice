7ec9908f0e3a28a4de46149b2fbc54f27ac7ba17
Colin Megill <colinmegill@gmail.com>
2016-05-17 23:42:50 -0700

    deps & build

---
+++ dev-server.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var express = require("express");
+var webpack = require("webpack");
+var config = require("./webpack.config.dev");
+var request = require("request");
+
+var app = express();
+var compiler = webpack(config);
+
+app.use(require("webpack-dev-middleware")(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require("webpack-hot-middleware")(compiler));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+var port = 4000;
+
+app.listen(port, "localhost", function(err) {
+  if (err) {
+    console.log("error", err);
+    return;
+  }
+
+  console.log("Listening at http://localhost:" + port);
+});
---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: ['eval','sourcemap'],
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'devel'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.json$/, loader: "json-loader"
+      }
+    ]
+  }
+};
