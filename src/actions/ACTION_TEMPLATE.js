c1a4f237a4e3835606e2281e6adedf3eff9299c8
Colin Megill <colinmegill@gmail.com>
2016-05-18 22:17:24 -0700

    refinements to action template

--- src/actions/ACTION_TEMPLATE.js
+++ src/actions/ACTION_TEMPLATE.js
@@ -7,31 +7,31 @@ export const BAZ_FETCH_ERROR = "BAZ_FETCH_ERROR";
 const requestFoo = () => {
   return {
     type: REQUEST_BAZ
-  }
+  };
 };
 
 const receiveFoo = (data) => {
   return {
     type: RECEIVE_BAZ,
     data: data
-  }
+  };
 };
 
 const barFetchError = (err) => {
   return {
     type: BAZ_FETCH_ERROR,
     data: err
-  }
-}
-
-const fetchFoo = (conversation_id) => {
-  return $.get("/api/v3/QUX?conversation_id=" + conversation_id);
+  };
 };
 
-export const populateFooStore = (conversation_id) => {
+const fetchFoo = () => {
+  return fetch("/asdfasdfasdfasdf");
+};
+
+export const populateFooStore = () => {
   return (dispatch) => {
     dispatch(requestFoo());
-    return fetchFoo(conversation_id).then(
+    return fetchFoo().then(
       (res) => dispatch(receiveFoo(res)),
       (err) => dispatch(barFetchError(err))
     );
