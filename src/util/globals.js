c48dfd783116a30a092e8390f23e29086a872406
Colin Megill <colinmegill@gmail.com>
2016-06-26 02:56:03 -0700

    globals exported from central file (if they are state --> reducer)

---
+++ src/util/globals.js
@@ -0,0 +1,13 @@
+export const date_select = true;
+export const file_prefix = "Zika_";
+export const branchLabels = false;
+export const restrictTo = {"region": "all"};
+export const time_window = 3.0;
+export const fullDataTimeWindow = 1.5;
+export const time_ticks = [2013.0, 2013.5, 2014.0, 2014.5, 2015.0, 2015.5, 2016.0];
+export const dfreq_dn = 2;
+export const LBItime_window = 0.5;
+export const LBItau = 0.0005;
+export const mutType = 'nuc';
+export const default_gene = 'nuc';
+export const plot_frequencies = false;
