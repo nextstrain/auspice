dc3e32316faeda152a0e74e02644d3879146ecd9
Colin Megill <colinmegill@gmail.com>
2016-07-09 19:12:43 -0700

    controls reducer gets defaults for colorby and scales

--- src/reducers/controls.js
+++ src/reducers/controls.js
@@ -1,8 +1,12 @@
 import * as types from "../actions/controls";
+import * as globals from "../util/globals"
+import getColorScale from "../util/getColorScale";
+
 
 const Controls = (state = {
   dateRange: null,
-  colorBy: null,
+  colorBy: globals.defaultColorBy,
+  colorScale: getColorScale(globals.defaultColorBy),
   showBranchLabels: false,
   region: null,
   search: null,
