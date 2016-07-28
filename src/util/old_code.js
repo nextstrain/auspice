2f953f5bac9bbfd7290ec1d4e10307c8e275ccc6
Colin Megill <colinmegill@gmail.com>
2016-07-28 16:12:56 -0700

    move old tooltip logic into comment in newly created tooltip component

--- src/util/old_code.js
+++ src/util/old_code.js
@@ -66,8 +66,6 @@
   });
 
 
-
-
   function dragged(d) {
 
     d.date = dateScale.invert(d3.event.x);
@@ -549,20 +547,7 @@
     styleHighlight();
   };
 
-  const markInTreeStrainSearch = (tip) => {
-    treeplot.selectAll(".strainmatch").data([tip])
-      .enter()
-      .append("text")
-      .attr("class", "strainmatch")
-      .text((d) => {
-        return "\uf069";
-      });
-      // .on('mouseover', function(d) {
-      //     virusTooltip.show(d, this);
-      // })
-      // .on('mouseout', virusTooltip.hide);
-    styleHighlight();
-  };
+
 
   const styleHighlight = () => {
     treeplot.selectAll(".seqmatch")
@@ -589,14 +574,7 @@
   /* bug duplicate */
   var searchEvent;
 
-  const highlightStrainSearch = (tip) => {
-    const strainName = (tip.strain).replace(/\//g, "");
-    d3.select("#" + strainName)
-      .call((d) => {
-        markInTreeStrainSearch(tip);
-        // virusTooltip.show(tip, d[0][0]);
-      });
-  };
+
 
   let strainSearchEvent;
 
@@ -797,189 +775,6 @@
     };
 
 
-  /*********************************
-  **********************************
-  **********************************
-  **********************************
-  ** Tooltips
-  **********************************
-  **********************************
-  **********************************
-  *********************************/
-
-  // var virusTooltip = d3.tip()
-  // 	.direction('se')
-  // 	.attr('class', 'd3-tip')
-  // 	.offset([0, 12])
-  // 	.html(function(d) {
-  //
-  // 		string = "";
-  //
-  // 		// safe to assume the following attributes
-  // 		if (typeof d.strain != "undefined") {
-  // 			string += d.strain;
-  // 		}
-  // 		string += "<div class=\"smallspacer\"></div>";
-  //
-  // 		string += "<div class=\"smallnote\">";
-  //
-  // 		// check if vaccine strain
-  // 		if (vaccineStrains.indexOf(d.strain) != -1) {
-  // 			string += "Vaccine strain<br>";
-  // 			var vaccine_date = new Date(vaccineChoice[d.strain]);
-  //
-  // 			string += "First chosen " + vaccine_date.toLocaleString("en-us", { month: "short" }) + " " + vaccine_date.getFullYear() + "<br>";
-  // 			string += "<div class=\"smallspacer\"></div>";
-  // 		}
-  //
-  // 		if (typeof d.country != "undefined") {
-  // 			string += d.country.replace(/([A-Z])/g, ' $1');
-  // 		}
-  // 		else if (typeof d.region != "undefined") {
-  // 			string += d.region.replace(/([A-Z])/g, ' $1');
-  // 		}
-  // 		if (typeof d.date != "undefined") {
-  // 			string += ", " + d.date;
-  // 		}
-  // 		if ((typeof d.db != "undefined") && (typeof d.accession != "undefined") && (d.db == "GISAID")) {
-  // 			string += "<br>GISAID ID: EPI" + d.accession;
-  // 		}
-  // 		if ((typeof d.db != "undefined") && (typeof d.accession != "undefined") && (d.db == "Genbank")) {
-  // 			string += "<br>Accession: " + d.accession;
-  // 		}
-  // 		if (typeof d.lab != "undefined") {
-  // 			if (d.lab != "") {
-  // 				string += "<br>Source: " + d.lab.substring(0,25);
-  // 				if (d.lab.length>25) string += '...';
-  // 			}
-  // 		}
-  // 		if (typeof d.authors != "undefined") {
-  // 			if (d.authors != "") {
-  // 				string += "<br>Authors: " + d.authors.substring(0,25);
-  // 				if (d.authors.length>25) string += '...';
-  // 			}
-  // 		}
-  // 		string += "</div>";
-  // 		// following may or may not be present
-  // 		if ((typeof focusNode != "undefined")){
-  // 			string += "<div class=\"smallspacer\"></div>";
-  // 			string += "HI against serum from "+focusNode.strain;
-  // 			string += "<div class=\"smallspacer\"></div>";
-  // 			string += "<div class=\"smallnote\">"
-  // 			string += '<table class="table table-condensed"><thead><tr><td>Serum</td><td>&#916log<sub>2</sub></td><td>heterol.</td><td>homol.</td></tr></thead><tbody>';
-  // 			if (typeof focusNode.HI_titers[d.clade] != "undefined"){
-  // 				for (var tmp_serum in focusNode.HI_titers[d.clade]){
-  // 					var autoHI = focusNode.autologous_titers[tmp_serum];
-  // 					var rawHI = focusNode.HI_titers_raw[d.clade][tmp_serum];
-  // 					var logHI = focusNode.HI_titers[d.clade][tmp_serum];
-  // 					if (correctVirus){logHI-=d.avidity_mut;}
-  // 					if (correctPotency){logHI-=focusNode.potency_mut[tmp_serum];}
-  // 					var serum_name;
-  // 					if (tmp_serum.length<20){
-  // 						serum_name = tmp_serum;
-  // 					}else{
-  // 						serum_name = tmp_serum.substring(0,17)+'...';
-  // 					}
-  // 					string += '<tr><td>' + serum_name + '</td><td>' +  logHI.toFixed(2) + '</td><td>' + rawHI.toFixed(0)+ '</td><td>' + autoHI.toFixed(0) +"</td></tr>";
-  // 				}
-  // 			}
-  // 			string += '<tr><td>' + 'Tree model' + '</td><td>' +  d.HI_dist_tree.toFixed(2) + '</td><td> --- </td><td>---</td></tr>';
-  // 			string += '<tr><td>' + 'Subs. model ' + '</td><td>' +  d.HI_dist_mut.toFixed(2) + '</td><td> --- </td><td>---</td></tr>';
-  // 			string += "</tbody></table></div>";
-  // 		}
-  //
-  // 		string += "<div class=\"smallspacer\"></div>";
-  // 		// following may or may not be present
-  // 		string += "<div class=\"smallnote\">";
-  // 		if (typeof d.cHI != "undefined") {
-  // 			string += "Antigenic adv: " + d.cHI.toFixed(1) + "<br>";
-  // 		}
-  // 		if (typeof d.ep != "undefined") {
-  // 			string += "Epitope distance: " + d.ep + "<br>";
-  // 		}
-  // 		if (typeof d.rb != "undefined") {
-  // 			string += "Receptor binding distance: " + d.rb + "<br>";
-  // 		}
-  // 		if (typeof d.LBI != "undefined") {
-  // 			string += "Local branching index: " + d.LBI.toFixed(3) + "<br>";
-  // 		}
-  // 		if (typeof d.dfreq != "undefined") {
-  // 			string += "Freq. change: " + d.dfreq.toFixed(3) + "<br>";
-  // 		}
-  // 		if (typeof d.fitness != "undefined") {
-  // 			string += "Fitness: " + d.fitness.toFixed(3) + "<br>";
-  // 		}
-  // 		if (typeof d.pred_distance != "undefined") {
-  // 			string += "Predicted distance: " + d.pred_distance.toFixed(3) + "<br>";
-  // 		}
-  // 		string += "</div>";
-  // 		return string;
-  // 	});
-
-
-  /* may be problematic in react */
-  // treeplot.call(virusTooltip);
-
-
-  // var linkTooltip = d3.tip()
-  // 	.direction('e')
-  // 	.attr('class', 'd3-tip')
-  // 	.offset([0, 12])
-  // 	.html(function(d) {
-  // 		string = ""
-  // 		if (typeof d.frequency != "undefined") {
-  // 			string += "Frequency: " + (100 * d.frequency).toFixed(1) + "%"
-  // 		}
-  // 		if (typeof d.dHI != "undefined") {
-  // 			string += "<br>Titer drop: " + d.dHI.toFixed(2)
-  // 		}
-  // 		string += "<div class=\"smallspacer\"></div>";
-  // 		string += "<div class=\"smallnote\">";
-  // 		if ((typeof d.aa_muts !="undefined")&&(mutType=='aa')){
-  // 			var ncount = 0;
-  // 			for (tmp_gene in d.aa_muts) {ncount+=d.aa_muts[tmp_gene].length;}
-  // 			if (ncount) {string += "<b>Mutations:</b><ul>";}
-  // 			for (tmp_gene in d.aa_muts){
-  // 				if (d.aa_muts[tmp_gene].length){
-  // 					string+="<li>"+tmp_gene+":</b> "+d.aa_muts[tmp_gene].replace(/,/g, ', ') + "</li>";
-  // 				}
-  // 			}
-  // 		}
-  // 		else if ((typeof d.nuc_muts !="undefined")&&(mutType=='nuc')&&(d.nuc_muts.length)){
-  // 			var tmp_muts = d.nuc_muts.split(',');
-  // 			var nmuts = tmp_muts.length;
-  // 			tmp_muts = tmp_muts.slice(0,Math.min(10, nmuts))
-  // 			string += "<li>"+tmp_muts.join(', ');
-  // 			if (nmuts>10) {string+=' + '+ (nmuts-10) + ' more';}
-  // 			string += "</li>";
-  // 		}
-  // 		string += "</ul>";
-  // 		if (typeof d.fitness != "undefined") {
-  // 			string += "Fitness: " + d.fitness.toFixed(3) + "<br>";
-  // 		}
-  // 		string += "click to zoom into clade"
-  // 		string += "</div>";
-  // 		return string;
-  // 	});
-  // treeplot.call(linkTooltip);
-  //
-  //
-  // var matchTooltip = d3.tip()
-  // 	.direction('e')
-  // 	.attr('class', 'd3-tip')
-  // 	.offset([0, 12])
-  // 	.html(function(d) {
-  // 		string = d.strain+ "<i> is closest match of:</i><ul>";
-  // 		string += "<div class=\"smallspacer\"></div>";
-  // 		for (var mi=0; mi<d.matches.length;mi++){
-  // 			string+="<li>" +d.matches[mi].substring(0,Math.min(30,d.matches[mi].length))+'</li>';
-  // 		}
-  // 		string += "</ul>";
-  // 		return string;
-  // 	});
-  // treeplot.call(matchTooltip);
-
-  /* this was in tree but leaving here for now as feels seperate */
 
   const exportTreeSVG = () => {
     const tmp = document.getElementById("treeplot-container");
