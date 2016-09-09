f82e7708a715b76148de96dec44947cab7263f04
Richard Neher <richard.neher@tuebingen.mpg.de>
2016-09-09 16:35:53 +0200

    added comments to virus-select

--- src/components/controls/choose-virus.js
+++ src/components/controls/choose-virus.js
@@ -11,6 +11,12 @@ import parseParams from "../../util/parseParams";
 // @connect(state => {
 //   return state.FOO;
 // })
+
+/*
+ * this component implements a series of selectors to select datasets.
+ * the dataset hierarchy is specified in a datasets.json, currently
+ * in ../../util/globals
+*/
 @Radium
 class ChooseVirus extends React.Component {
   constructor(props) {
@@ -38,21 +44,27 @@ class ChooseVirus extends React.Component {
       }
     };
   }
+
   render() {
     const styles = this.getStyles();
-    let options=['default'];
-    let selectors = [];
-    let level = datasets;
+
+    // remove starting or trailing slashes from path
     let tmppath = (this.props.location.pathname[0]=='/')?this.props.location.pathname.substring(1):this.props.location.pathname;
     tmppath = (tmppath[tmppath.length-1]=='/')?tmppath.substring(0,tmppath.length-1):tmppath;
-    const config = parseParams(tmppath)['dataset']
-    const fields = Object.keys(config).sort( (a,b) => config[a][0]>config[b][0]);
-    const choices = fields.map((d) => config[d][1]);
-    // console.log('control:', this.props, fields, choices);
+    // analyse the current route in order to adjust the dataset selection choices
+    const parsedParams = parseParams(tmppath)['dataset']
+    // names of the different selectors in the current hierarchy: [virus, lineage, duration]
+    const fields = Object.keys(parsedParams).sort( (a,b) => parsedParams[a][0]>parsedParams[b][0]);
+    // the current choices: [flu, h3n2, 3y]
+    const choices = fields.map((d) => parsedParams[d][1]);
+
+    // make a selector for each of the fields
+    let selectors = [];   // list to contain the different data set selectors
+    let level = datasets; // pointer used to move through the hierarchy -- currently at the top level of datasets
     for (let vi=0; vi<fields.length; vi++){
       if (choices[vi]){
-        options = Object.keys(level[fields[vi]]).filter((d) => d!='default');
-        // console.log(fields[vi], choices[vi], options, choices.slice(0,vi+1));
+        // pull options from the current level of the dataset hierarchy, ignore 'default'
+        const options = Object.keys(level[fields[vi]]).filter((d) => d!='default');
         selectors.push((
           <div style={[
             styles.base,
@@ -66,10 +78,11 @@ class ChooseVirus extends React.Component {
               options={options}/>
             </div>
           ));
+        // move to the next level in the data set hierarchy
         level = level[fields[vi]][choices[vi]];
       }
     }
-    // console.log(selectors);
+    // return a list of selectors in the order of the data set hierarchy
     return (
       <div>
         {selectors}
@@ -77,8 +90,5 @@ class ChooseVirus extends React.Component {
       );
   }
 }
-
-//
-// {this.props.query.strain ? this.renderDurationSelect() : null}
 
 export default ChooseVirus;
