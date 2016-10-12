7c9f311605e9645d592e135d98b215098b762962
Colin Megill <colinmegill@gmail.com>
2016-10-12 12:11:44 -0700

    switch to mapbox tiles

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -37,7 +37,7 @@ class Map extends React.Component {
       zoom: 2
     })
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/trvrb/ciu03v244002o2in5hlm3q6w2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidHJ2cmIiLCJhIjoiY2l1MDRoMzg5MDEwbjJvcXBpNnUxMXdwbCJ9.PMqX7vgORuXLXxtI3wISjw', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
