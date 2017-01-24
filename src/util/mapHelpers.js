a553bf0a40ae8b8878426cf28e63bb5849045c88
Colin Megill <colinmegill@gmail.com>
2017-01-24 12:48:43 -0800

    break out latlong into its own helper, d3ify map

--- src/util/mapHelpers.js
+++ src/util/mapHelpers.js
@@ -1,157 +1,77 @@
-export const addAllTipsToMap = (nodes, metadata, colorScale, map) => {
+import getLatLongs from "./mapHelpersLatLong";
 
-  const aggregatedLocations = {};
+export const setupTipsAndTransmissions = (nodes, metadata, colorScale, map, responsive, handleAnimationPlayClicked) => {
 
-  setTimeout(() => {
-    const mapSVG = d3.select("#map").select("svg").append("g");
+  /*
+    data structures to feed to d3
+    latLongs = {
+      tips: [{}, {}],
+      transmissions: [{}, {}]
+    }
+  */
+  const latLongs = getLatLongs(nodes, metadata, map);
 
-    const latLongs = [];
 
-    nodes.forEach((n) => {
-      if (n.children) { return; }
-      // look up geo1 geo2 geo3 do lat longs differ
-      if (aggregatedLocations[n.attr.country]) {
-        aggregatedLocations[n.attr.country]++;
-      } else {
-        // if we haven't added this pair, add it
-        aggregatedLocations[n.attr.country] = 1;
-      }
-    });
+  /*
+    add circles and lines to map, add event listeners for leaflet zooming
+  */
 
-    _.forOwn(aggregatedLocations, (value, key) => {
-      latLongs.push({
-        country: key, // Thailand:
-        total: value, // 20
-        coords: map.latLngToLayerPoint( /* interchange. this is a leaflet method that will tell d3 where to draw. */
-          new L.LatLng(
-            metadata.geo.country[key].latitude,
-            metadata.geo.country[key].longitude
-          )
-        )
-      });
-    });
+    const mapSVG = d3.select(map.getPanes().overlayPane).append("svg").attr("width", responsive.width).attr("height", responsive.height);
+    const d3Group = mapSVG.append("g").attr("class", "leaflet-zoom-hide");
 
-    const tips = mapSVG.selectAll("tips")
-      .data(latLongs)
+    const tips = d3Group.selectAll("tips")
+      .data(latLongs.tips)
       .enter().append("circle")
       .style("stroke", "none")
       .style("fill-opacity", .6)
       .style("fill", (d) => { return colorScale(d.country) })
-      .attr("r", (d) => { return 2 + Math.sqrt(d.total) * 4 });
+      .attr("r", (d) => { console.log('adf', d); return 2 + Math.sqrt(d.total) * 4 });
+
+    const play = d3Group.append("circle")
+      .attr("width", 40)
+      .attr("height", 40)
+      .style("background-color", "green")
+      .on("click", handleAnimationPlayClicked)
+
+      /* this will need to be scaled if transmissions is high */
+      // const arrowSizeMultiplier = value > 1 ? value * 2 : 0;
+
+      // this decorator adds arrows to the lines.
+      // since we're now using d3 we'll probably do something like http://stackoverflow.com/questions/11808860/arrow-triangles-on-my-svg-line/11809868#11809868
+      // decorator docs: https://github.com/bbecquet/Leaflet.PolylineDecorator
+      // for (let i = 0; i < geodesicPath._latlngs.length; i++) {
+      //   L.polylineDecorator(geodesicPath._latlngs[i], {
+      //     patterns: [{
+      //       offset: 25,
+      //       repeat: 50,
+      //       symbol: L.Symbol.arrowHead({
+      //         pixelSize: 8 + arrowSizeMultiplier,
+      //         pathOptions: {
+      //           fillOpacity: .5,
+      //           color: colorScale(countries[0]),
+      //           weight: 0
+      //         }
+      //       })
+      //     }]
+      //   }).addTo(map);
+      // }
 
     const setTipCoords = () => {
-      // tips.attr("cx", (d) => { return d.coords.x })
-      //   .attr("cy", (d) => { return d.coords.y })
-
-      tips.attr("transform",
-        (d) => {
-          return "translate(" +
-            d.coords.x + ","+
-            d.coords.y + ")";
-        }
-      )
-    }
+      tips.attr("transform", (d) => {
+        return "translate(" + d.coords.x + "," + d.coords.y + ")";
+      });
+    };
 
     setTipCoords();
-    map.on("viewreset", setTipCoords);
+    map.on("viewreset", setTipCoords); /* search: -Note (A) for an idea as to why this might not be working properly */
 
-  }, 0) // end setTimout to wait for leaflet in the DOM
+  // return {}; this will probably return tips & transmissions to store in state, so that we can pass them to whatever helper animates them
 
 }
 
+export const animateTransmissions = (nodes, metadata, colorScale, map) => {
 
-// var start = null;
-// var element = document.getElementById("SomeElementYouWantToAnimate");
-// element.style.position = 'absolute';
-//
-// function step(timestamp) {
-//   if (!start) start = timestamp;
-//   var progress = timestamp - start;
-//   element.style.left = Math.min(progress/10, 200) + "px";
-//   if (progress < 2000) {
-//     window.requestAnimationFrame(step);
-//   }
-// }
-//
-// window.requestAnimationFrame(step);
+  /* projection tween http://bl.ocks.org/mbostock/3711652 */
+  /* point along path interpolation https://bl.ocks.org/mbostock/1705868 */
 
-
-
-export const addTransmissionEventsToMap = (nodes, metadata, colorScale, map) => {
-  const transmissions = {};
-  const geo = metadata.geo;
-
-  // count transmissions for line thickness
-  nodes.forEach((parent) => {
-    if (!parent.children) { return; }
-    // if (parent.attr.country !== "china") { return; } // remove me, example filter
-    parent.children.forEach((child) => {
-      if (parent.attr.country === child.attr.country) { return; }
-      // look up in transmissions dictionary
-      if (transmissions[parent.attr.country + "/" + child.attr.country]) {
-        transmissions[parent.attr.country + "/" + child.attr.country]++;
-      } else {
-        // we don't have it, add it
-        transmissions[parent.attr.country + "/" + child.attr.country] = 1;
-      }
-    });
-  });
-
-  // for each item in the object produced above, add a line
-  _.forOwn(transmissions, (value, key) => {
-
-    // go from "brazil/cuba" to ["brazil", "cuba"]
-    const countries = key.split("/");
-    // go from "brazil" to lat0 = -14.2350
-    let long0 = geo.country[countries[0]].longitude;
-    let long1 = geo.country[countries[1]].longitude;
-    let lat0 = geo.country[countries[0]].latitude;
-    let lat1 = geo.country[countries[1]].latitude;
-
-    // create new leaflet LatLong objects
-    const start = new L.LatLng(lat0, long0)
-    const end = new L.LatLng(lat1, long1)
-
-    // remove me! temporary random colors in lieu of scale.
-
-    /*
-      add a polyline to the map for current country pair iteratee
-      store the computation. access _latlngs to show where each segment is on the map
-    */
-    const geodesicPath = L.geodesic([[start,end]], {
-      // stroke:	value,
-      // radius: value,
-      color: colorScale(countries[0]),
-      opacity: .5,
-      steps: 25,
-      weight:	value	/* Stroke width in pixels.*/
-      // opacity:	0.5	Stroke opacity.
-      // fill:
-      // fillColor: randomColor
-      // fillOpacity:
-    }).addTo(map)
-
-    /* this will need to be scaled if transmissions is high */
-    const arrowSizeMultiplier = value > 1 ? value * 2 : 0;
-
-    // this decorator adds arrows to the lines.
-    // decorator docs: https://github.com/bbecquet/Leaflet.PolylineDecorator
-    // for (let i = 0; i < geodesicPath._latlngs.length; i++) {
-    //   L.polylineDecorator(geodesicPath._latlngs[i], {
-    //     patterns: [{
-    //       offset: 25,
-    //       repeat: 50,
-    //       symbol: L.Symbol.arrowHead({
-    //         pixelSize: 8 + arrowSizeMultiplier,
-    //         pathOptions: {
-    //           fillOpacity: .5,
-    //           color: colorScale(countries[0]),
-    //           weight: 0
-    //         }
-    //       })
-    //     }]
-    //   }).addTo(map);
-    // }
-
-  });
 }
---
+++ src/util/mapHelpersLatLong.js
@@ -0,0 +1,107 @@
+const setupLatLong = (nodes, metadata, map) => {
+
+  const aggregatedLocations = {}; /* tips */
+  const transmissions = {};
+  const tipsAndTransmissions = {
+    tips: [],
+    transmissions: []
+  };
+  /*
+    edges / transmissions
+  */
+  const geo = metadata.geo;
+
+  /*
+    aggregate locations for tips
+  */
+  nodes.forEach((n) => {
+    if (n.children) { return; }
+    // look up geo1 geo2 geo3 do lat longs differ
+    if (aggregatedLocations[n.attr.country]) {
+      aggregatedLocations[n.attr.country]++;
+    } else {
+      // if we haven't added this pair, add it
+      aggregatedLocations[n.attr.country] = 1;
+    }
+  });
+
+  /*
+    create a latlong pair for each country's location and push them all to a common array
+  */
+  _.forOwn(aggregatedLocations, (value, key) => {
+    tipsAndTransmissions.tips.push({
+      country: key, // Thailand:
+      total: value, // 20
+      coords: map.latLngToLayerPoint( /* interchange. this is a leaflet method that will tell d3 where to draw. -Note (A) we MAY have to do this every time rather than just once */
+        new L.LatLng(
+          metadata.geo.country[key].latitude,
+          metadata.geo.country[key].longitude
+        )
+      )
+    });
+  });
+
+  /*
+    count transmissions for line thickness
+  */
+  nodes.forEach((parent) => {
+    if (!parent.children) { return; }
+    // if (parent.attr.country !== "china") { return; } // remove me, example filter
+    parent.children.forEach((child) => {
+      if (parent.attr.country === child.attr.country) { return; }
+      // look up in transmissions dictionary
+      if (transmissions[parent.attr.country + "/" + child.attr.country]) {
+        transmissions[parent.attr.country + "/" + child.attr.country]++;
+      } else {
+        // we don't have it, add it
+        transmissions[parent.attr.country + "/" + child.attr.country] = 1;
+      }
+    });
+  });
+
+  // for each item in the object produced above, add a line
+  _.forOwn(transmissions, (value, key) => {
+
+    // go from "brazil/cuba" to ["brazil", "cuba"]
+    const countries = key.split("/");
+    // go from "brazil" to lat0 = -14.2350
+    let long0 = geo.country[countries[0]].longitude;
+    let long1 = geo.country[countries[1]].longitude;
+    let lat0 = geo.country[countries[0]].latitude;
+    let lat1 = geo.country[countries[1]].latitude;
+
+    // create new leaflet LatLong objects
+    const start = new L.LatLng(lat0, long0)
+    const end = new L.LatLng(lat1, long1)
+
+    /*
+      add a polyline to the map for current country pair iteratee
+      store the computation. access _latlngs to show where each segment is on the map
+    */
+    const geodesic = L.geodesic([[start,end]], {
+      // stroke:	value,
+      // radius: value,
+      // color: colorScale(countries[0]), /* this will go up above in d3 rather than in leaflet now */
+      // opacity: .5,
+      // steps: 25,
+      // weight:	value	/* Stroke width in pixels.*/
+      // opacity:	0.5	Stroke opacity.
+      // fill:
+      // fillColor: randomColor
+      // fillOpacity:
+    })
+
+    tipsAndTransmissions.transmissions.push({
+      start,
+      end,
+      geodesic,
+      from: countries[0],
+      to: countries[1]
+    })
+
+  });
+
+  return tipsAndTransmissions;
+}
+
+export default setupLatLong
