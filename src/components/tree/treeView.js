nextstrain/auspice

commit 9688cabf6109df00d8df5ab6bf59fcac60fe2500

infoPanel dissapears on mouseout, closes #130

src/components/tree/treeView.js
@@ -46,6 +46,8 @@ class TreeView extends React.Component {
     this.state = {
       okToDraw: false,
       tool: "pan",  //one of `none`, `pan`, `zoom`, `zoom-in`, `zoom-out`
+      clicked: null,
+      hover: null
     };
   }
   componentWillMount() {
@@ -196,7 +198,8 @@ class TreeView extends React.Component {
           onTipHover: this.onTipHover.bind(this),
           onTipClick: this.onTipClick.bind(this),
           onBranchHover: this.onBranchHover.bind(this),
-          onBranchClick: this.onBranchClick.bind(this)
+          onBranchClick: this.onBranchClick.bind(this),
+          onBranchOrTipLeave: this.onBranchOrTipLeave.bind(this)
         },
         {
           /* presently selected node / branch */
@@ -251,6 +254,13 @@ class TreeView extends React.Component {
       hovered: null
     });
   }
+  // mouse out from tip/branch
+  onBranchOrTipLeave(){
+    if (this.state.hovered) {
+      this.setState({hovered: null})
+    }
+  }
+
   handleIconClick(tool) {
     return () => {
 

src/util/phyloTree.js
@@ -782,6 +782,9 @@ PhyloTree.prototype.makeTips = function() {
     .on("mouseover", (d) => {
       this.callbacks.onTipHover(d)
     })
+    .on("mouseout", () => {
+      this.callbacks.onBranchOrTipLeave()
+    })
     .on("click", (d) => {
       this.callbacks.onTipClick(d)
     })
@@ -812,6 +815,9 @@ PhyloTree.prototype.makeBranches = function() {
     })
     .on("mouseover", (d) => {
       this.callbacks.onBranchHover(d)
+    })
+    .on("mouseout", () => {
+      this.callbacks.onBranchOrTipLeave()
     })
     .on("click", (d) => {
       this.callbacks.onBranchClick(d)

commit 3971c15efcafac7f24a6a6e0e9e93f5429c9acb6

Restore dashed lines on tree
Dashed lines on hover broke in commit 9688cab

src/components/tree/treeView.js
@@ -134,30 +134,39 @@ class TreeView extends React.Component {
     }
   }
   componentWillUpdate(nextProps, nextState) {
-    /* reconcile hover and click selections in tree */
+    /* reconcile hover and click selections in tree
+       that is, make branches / tips dashed or not...
+       updateSelectedBranchOrTip args: oldSelected, newSelected
+       where each is an object with attrs d (node) and type (branch/tip)
+       OR arguments could by falsey
+       That function checks whether the branch (eg) is the same (if so: no-op)
+    */
     if (
       this.state.tree &&
-      (this.state.hovered || this.state.clicked) &&
+      ((this.state.hovered || this.state.clicked) || (!this.state.hovered && nextState.hovered)) &&
       this.props.layout === nextProps.layout // this block interferes with layout transition otherwise
     ) {
       /* check whether or not the previously selected item was clicked */
-      if (this.state.clicked && nextState.clicked) { // was the previous item a click?
+      if (this.state.clicked && nextState.clicked) {
+	// console.log("click -> click");
         this.state.tree.updateSelectedBranchOrTip(
           this.state.clicked, /* turn this one off */
           nextState.clicked, /* turn this one on */
         );
-      } else if (this.state.hovered && nextState.clicked) { // previously a hover, now a click
+      } else if (!this.state.hovered && nextState.hovered) {
+	// console.log("not hovered -> hovered");
+        this.state.tree.updateSelectedBranchOrTip(
+	  null,
+	  nextState.hovered,
+        );
+      } else if (this.state.hovered && !nextState.hovered && !nextState.clicked) {
+	// console.log("hovered -> no longer hovered or clicked");
         this.state.tree.updateSelectedBranchOrTip(
           this.state.hovered,
-          nextState.clicked,
+	  null,
         );
-      } else if (this.state.hovered && nextState.hovered) { // deselect the previously selected hover
-        this.state.tree.updateSelectedBranchOrTip(
-          this.state.hovered,
-          nextState.hovered,
-        );
-      }
-      else if (this.state.clicked && nextState.clicked === null) {
+      } else if (this.state.clicked && nextState.clicked === null) {
+	// console.log("click -> noClick");
         // x clicked or clicked off will give a null value, so reset everything to be safe
         this.state.tree.updateSelectedBranchOrTip(
           this.state.clicked,
@@ -389,6 +398,8 @@ class TreeView extends React.Component {
       </Card>
     )
   }
+
+
   render() {
     /*
       1. set up SVGs

src/util/phyloTree.js
@@ -630,16 +630,20 @@ PhyloTree.prototype.deSelectTip = function(node) {
     .style("fill", function(d) { return d.fill;});
 };
 
-
+/* eslint-disable no-unused-expressions */
+/* eslint-disable max-len */
 PhyloTree.prototype.updateSelectedBranchOrTip = function (oldSelected, newSelected) {
-  if (!newSelected || !newSelected || oldSelected.d.n.clade !== newSelected.d.n.clade){
-    if (oldSelected) this.deSelectBranch(oldSelected.d);
-    if (newSelected && newSelected.type===".branch") this.selectBranch(newSelected.d);
-
-    if (oldSelected) this.deSelectTip(oldSelected.d);
-    if (newSelected && newSelected.type===".tip") this.selectTip(newSelected.d);
+  if (oldSelected === null && newSelected !== null) { // mouse in
+    newSelected.type === ".branch" ? this.selectBranch(newSelected.d) : this.selectTip(newSelected.d);
+  } else if (oldSelected !== null && newSelected === null) { // mouse out
+    oldSelected.type === ".branch" ? this.deSelectBranch(oldSelected.d) : this.deSelectTip(oldSelected.d);
+  } else if (oldSelected.d.n.clade !== newSelected.d.n.clade) { // new click
+    oldSelected.type === ".branch" ? this.deSelectBranch(oldSelected.d) : this.deSelectTip(oldSelected.d);
+    newSelected.type === ".branch" ? this.selectBranch(newSelected.d) : this.selectTip(newSelected.d);
   }
 };
+/* eslint-enable no-unused-expressions */
+/* eslint-enable max-len */
 
 /*
  * update tree element style of attributes