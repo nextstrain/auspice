ebacedfd0ba3ddc91bd826de2629f7854df7c607
James Hadfield <jh22@sanger.ac.uk>
2017-01-28 11:57:45 -0800

    remove more unused npm packages

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import { devTools } from 'redux-devtools';
-import createHistory from 'history/lib/createBrowserHistory';
+// import { devTools } from 'redux-devtools';
+// import createHistory from 'history/lib/createBrowserHistory';
 
 import rootReducer from "../reducers";
 
