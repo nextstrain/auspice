45f32db7b32157b210209bee82c347ecacbeac0e
James Hadfield <jh22@sanger.ac.uk>
2017-02-01 18:53:11 -0800

    provide colors for incomplete metadata colours
    This uses a grayscale ramp to provide colours for those countries appearing in the tree but not given colours in metadata. This is only for situations where *some* countries have been given colours.

--- src/util/getColorScale.js
+++ src/util/getColorScale.js
@@ -4,6 +4,19 @@ import { genericDomain, colors, genotypeColors } from "./globals";
 import { parseGenotype, getGenotype } from "./getGenotype"
 import d3 from "d3";
 
+/* this checks if there are more items in the tree compared
+   to associated colours in the metadata JSON
+*/
+const getExtraVals = (nodes, colorBy, color_map) => {
+  let valsInTree = []
+  nodes.forEach((n)=>valsInTree.push(n.attr[colorBy]))
+  valsInTree = [...new Set(valsInTree)]
+  const valsInMeta = color_map.map((d) => { return d[0]})
+  // only care about values in tree NOT in metadata
+  return valsInTree.filter((x) => valsInMeta.indexOf(x) === -1)
+}
+
+
 const genericScale = (cmin, cmax) => {
   const offset = +cmin;
   const range = cmax - cmin;
@@ -37,11 +50,12 @@ const integerAttributeScale = (nodes, attr) => {
    this is necessary as ordinal scales can't interpololate colours.
    it would be cool to use chroma, but i couldn't import it properly :(
    http://gka.github.io/chroma.js/
+   range: [a,b], the colours to go between
 */
-const createListOfColors = (n) => {
-  const scale = d3.scale.linear().domain([1, n])
+const createListOfColors = (n, range) => {
+  const scale = d3.scale.linear().domain([0, n])
       .interpolate(d3.interpolateHcl)
-      .range([d3.rgb("#007AFF"), d3.rgb("#FFD000")]);
+      .range(range);
   return d3.range(0, n).map(scale);
 };
 
@@ -54,7 +68,7 @@ const discreteAttributeScale = (nodes, attr) => {
   domain.sort((a, b) => stateCount[a] > stateCount[b]);
   return d3.scale.ordinal()
                  .domain(domain)
-                 .range(createListOfColors(domain.length));
+                 .range(createListOfColors(domain.length, [d3.rgb("#007AFF"), d3.rgb("#FFD000")]));
 };
 
 const getColorScale = (colorBy, tree, sequences, colorOptions) => {
@@ -86,10 +100,19 @@ const getColorScale = (colorBy, tree, sequences, colorOptions) => {
   } else if (colorOptions && colorOptions[colorBy]) {
     if (colorOptions[colorBy].color_map) {
       // console.log("Sweet - we've got a color_map for ", colorBy)
+      let domain = colorOptions[colorBy].color_map.map((d) => { return d[0]; })
+      let range = colorOptions[colorBy].color_map.map((d) => { return d[1]; })
+      const extraVals = getExtraVals(tree.nodes, colorBy, colorOptions[colorBy].color_map)
+      if (extraVals.length) {
+        // we must add these to the domain + provide a value in the range
+        domain = domain.concat(extraVals);
+        const extrasColorAB = [d3.rgb(192, 192, 192), d3.rgb(32, 32, 32)]
+        range = range.concat(createListOfColors(extraVals.length, extrasColorAB))
+      }
       continuous = false;
       colorScale = d3.scale.ordinal()
-        .domain(colorOptions[colorBy].color_map.map((d) => { return d[0]; }))
-        .range(colorOptions[colorBy].color_map.map((d) => { return d[1]; }));
+                           .domain(domain)
+                           .range(range);
     } else if (colorOptions && colorOptions[colorBy].type === "discrete") {
       // console.log("making a discrete color scale for ", colorBy)
       continuous = false;
