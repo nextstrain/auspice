8066e2a1e32e8fd4386d96c836df452fe3e6bc56
Colin Megill <colinmegill@gmail.com>
2017-02-10 01:01:41 -0800

    refactor map.js, call latlong directly, store d3node in state, methods

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Radium from "radium";
+import d3 from "d3";
 import _ from "lodash";
-// import Flex from "./framework/flex";
 import { connect } from "react-redux";
-// import { FOO } from "../actions";
 import Card from "../framework/card";
 import setupLeaflet from "../../util/leaflet";
 import setupLeafletPlugins from "../../util/leaflet-plugins";
-import {setupTipsAndTransmissions} from "../../util/mapHelpers";
+import {drawTipsAndTransmissions} from "../../util/mapHelpers";
 import * as globals from "../../util/globals";
 import computeResponsive from "../../util/computeResponsive";
+import getLatLongs from "../../util/mapHelpersLatLong";
 import {
   MAP_ANIMATION_TICK,
   MAP_ANIMATION_END
@@ -19,6 +18,7 @@ import {
   return {
     tree: state.tree.tree,
     metadata: state.metadata.metadata,
+    colorBy: state.controls.colorBy,
     browserDimensions: state.browserDimensions.browserDimensions,
     map: state.map
   };
@@ -27,21 +27,20 @@ class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tips: false,
       map: null,
+      tips: false,
+      d3DOMNode: null,
+      d3elems: null,
       datasetGuid: null,
-      responsive: null
+      responsive: null,
     };
   }
   componentWillMount() {
-    /* this sets up window.L */
-    setupLeaflet();
+    setupLeaflet(); /* this sets up window.L */
   }
   componentDidMount() {
-    /* this attaches several properties to window.L */
-    setupLeafletPlugins();
+    setupLeafletPlugins(); /* this attaches several properties to window.L */
   }
-
   componentWillReceiveProps(nextProps) {
     /*
       React to browser width/height changes responsively
@@ -74,67 +73,128 @@ class Map extends React.Component {
       this.setState({responsive})
     }
   }
-
   componentDidUpdate(prevProps, prevState) {
-
-    /* first time map */
+    this.maybeSetupLeaflet(); /* puts leaflet in the DOM, only done once */
+    this.maybeSetupD3DOMNode(); /* attaches the D3 SVG DOM node to the Leaflet DOM node, only done once */
+    this.maybeDrawTipsAndTransmissions(); /* it's the first time, or they were just removed because we changed dataset */
+    this.maybeUpdateTipsAndTransmissions(); /* every time we change something like colorBy */
+    this.maybeAnimateTipsAndTransmissions();
+    this.maybeRemoveAllTipsAndTransmissions(prevProps); /* dataset just changed */
+  }
+  maybeSetupLeaflet() {
+    /* first time map, this sets up leaflet */
     if (
       this.props.browserDimensions &&
+      this.props.metadata &&
       !this.state.map
     ) {
-      console.info("creating map")
       this.createMap();
     }
-
+  }
+  maybeSetupD3DOMNode() {
     if (
-      this.state.map && // we have a map
-      prevProps.datasetGuid &&
-      this.props.datasetGuid &&
-      prevProps.datasetGuid !== this.props.datasetGuid // and the dataset has changed
+      this.state.map &&
+      this.state.responsive &&
+      !this.state.d3DOMNode
     ) {
-      console.info("removing map")
-      this.state.map.remove()
-      this.setState({
-        map: null,
-        tips: false
-      })
+      /* add circles and lines to map, add event listeners for leaflet zooming */
+      const mapSVG = d3.select(this.state.map.getPanes().overlayPane)
+        .append("svg")
+        .attr("width", this.state.responsive.width)
+        .attr("height", this.state.responsive.height);
+      const g = mapSVG.append("g").attr("class", "leaflet-zoom-hide");
 
+      this.setState({d3DOMNode: g})
     }
-
+  }
+  maybeDrawTipsAndTransmissions() {
     if (
       this.props.colorScale &&
       this.state.map && /* we have already drawn the map */
       this.props.metadata && /* we have the data we need */
       this.props.nodes &&
       this.state.responsive &&
+      this.state.d3DOMNode &&
       !this.state.tips /* we haven't already drawn tips */
     ) {
-      setupTipsAndTransmissions(
-        this.props.nodes,
-        this.props.metadata,
+      /* data structures to feed to d3 latLongs = { tips: [{}, {}], transmissions: [{}, {}] } */
+      const latLongs = this.latLongs();
+
+      const d3elems = drawTipsAndTransmissions(
+        latLongs,
         this.props.colorScale,
-        this.state.map,
-        this.state.responsive
+        this.state.d3DOMNode,
       );
+      // this.state.map.on("viewreset", this.drawOverlay.bind(this));
+      // this.state.map.on("moveend", this.drawOverlay.bind(this));
+
       // don't redraw on every rerender - need to seperately handle virus change redraw
       this.setState({
-        tips: true
+        tips: true,
+        d3elems,
       });
     }
-
   }
-  /******************************************
-   * GET LEAFLET IN THE DOM
-   *****************************************/
+  maybeUpdateTipsAndTransmissions() {
+    /* todo */
+  }
+  maybeAnimateTipsAndTransmissions() {
+    /* todo */
+  }
+  maybeRemoveAllTipsAndTransmissions(prevProps) {
+    /* dataset change, remove all tips and transmissions d3 added */
+    if (
+      this.state.map && // we have a map
+      prevProps.datasetGuid &&
+      this.props.datasetGuid &&
+      prevProps.datasetGuid !== this.props.datasetGuid // and the dataset has changed
+    ) {
+      this.state.d3DOMNode.selectAll("*").remove();
+
+      /* clear references to the tips and transmissions d3 added */
+      this.setState({
+        map: null,
+        tips: false,
+        d3elems: null,
+        latLongs: null,
+      })
+    }
+  }
+  latLongs() {
+    return getLatLongs(
+      this.props.nodes,
+      this.props.metadata,
+      this.state.map,
+      this.props.colorBy
+    );
+  }
   createMap() {
+    /******************************************
+    * GET LEAFLET IN THE DOM
+    *****************************************/
 
     const southWest = L.latLng(-70, -180);
     const northEast = L.latLng(80, 180);
     const bounds = L.latLngBounds(southWest, northEast);
+    let zoom = 2;
+    let center = [0,0];
+
+    /*
+      hardcode. this will last a while.
+      when we want to dynamically calculate the bounds,
+      map will have to know about the path latlongs calculated in maphelpers.
+      not at all sure how we'll do that and account for great circle paths.
+    */
+    if (window.location.pathname.indexOf("ebola") !== -1) {
+      zoom = 7;
+      center = [8, -11];
+    } else if (window.location.pathname.indexOf("zika") !== -1) {
+      /* zika is fine at the default settings */
+    }
 
     var map = L.map('map', {
-      center: [0,0],
-      zoom: 2,
+      center: center,
+      zoom: zoom,
       scrollWheelZoom: false,
       maxBounds: bounds,
       minZoom: 2,
@@ -162,7 +222,6 @@ class Map extends React.Component {
 
     this.setState({map});
   }
-
   createMapDiv() {
     return (
       <div style={{position: "relative"}}>
@@ -189,13 +248,12 @@ class Map extends React.Component {
       </div>
     )
   }
-  /******************************************
-   * ANIMATE MAP (AND THAT LINE ON TREE)
-   *****************************************/
-  handleAnimationPlayClicked() { /* this will all probably go down into components/map/map.js */
+  handleAnimationPlayClicked() {
+    /******************************************
+    * ANIMATE MAP (AND THAT LINE ON TREE)
+    *****************************************/
     this.animateMap();
   }
-
   animateMap() {
     let start = null;
 
@@ -220,7 +278,6 @@ class Map extends React.Component {
 
       window.requestAnimationFrame(step);
   }
-
   render() {
     // console.log('map sees', this.props.map)
     // clear layers - store all markers in map state https://github.com/Leaflet/Leaflet/issues/3238#issuecomment-77061011
