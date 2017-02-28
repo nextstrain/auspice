f866158f12ac347242832fbe4a40e837d1c37e7f
Trevor Bedford <trevor@bedford.io>
2017-02-27 19:25:47 -0500

    Revise methods. Fixes #145.

--- src/static/methods.js
+++ src/static/methods.js
@@ -13,8 +13,6 @@ const Methods = () => {
 
 			<div className="static container">
 
-
-
         <div className="row">
           <div className="col-md-1"/>
           <div className="col-md-7">
@@ -28,21 +26,24 @@ const Methods = () => {
           <div className="col-md-1"/>
 					<div className="col-md-6">
 
-						The data processing for nextstrain consists of two stages: data collection (known as <a href="https://github.com/nextstrain/fauna">fauna</a>) and data processing (known as <a href="https://github.com/nextstrain/augur">augur</a>), each publicly available and detailed on github. Here we give an overview of these methods. The data vizualisation code is known as <a href="https://github.com/nextstrain/auspice">auspice</a>.
+            The nextstrain architecture consists of three components. We maintain a database (<a href="https://github.com/nextstrain/fauna">fauna</a>) with associated ingest scripts to scrape publically available sequences and clean up metadata. Cleaned sequences are passed from the database to
+            our bioinformatic processing pipeline (<a href="https://github.com/nextstrain/augur">augur</a>) that aligns sequences, constructs phylogenies, infers a temporal scale and reconstructs geographic transmission history. The informatic scripts are handled offline and are rerun as new data appears in public databases. The resulting processed data is uploaded as a package that is queried by our client-side visualization (<a href="https://github.com/nextstrain/auspice">auspice</a>).
 
             <h2>Data Collection</h2>
 
-						We maintain a database of publicly available sequences and have written a number of scripts to ingest and canonicize data from different sources. This database, which is regularly updated, allows downloading of all zika, ebola and influenza genomes in fasta format.
+						We maintain a database of publicly available sequences and have written a number of scripts to ingest and canonicize data from different sources. This database, which is regularly updated, allows downloading of Zika, Ebola and influenza genomes in fasta format.
 
-						<h2>Pruning and Alignment</h2>
+						<h2>Filtering</h2>
 
-						The first step in the processing pipeline is to automatically select a subset of representative viruses. This subsampling step is virus depended. For influenza virus, sequences without complete date or geographic information, viruses passaged in eggs and sequences less than 987 bases are removed. In addition, local outbreaks are filtered by keeping only one instance of identical sequences sampled at the same location on the same day. Following filtering, the viruses are subsampled to achieve a more equitable temporal and geographic distribution. For our standard display period of 3 years and 32 viruses per month, this typically results in ~1200 viruses. In the case of Zika or Ebola virus, substantial subsampling is not necessary and we mostly remove low quality sequences or duplicates.<br></br>
+						The first step in the processing pipeline is to automatically select a subset of representative viruses. This subsampling step is virus dependent. For influenza virus, sequences are subsampled to achieve a more equitable temporal and geographic distribution. In the case of Zika or Ebola virus, substantial subsampling is not necessary and we mostly remove low quality sequences or duplicates. Here, we also remove duplicate viruses and known reassortant clusters in influenza.
 
-            Augur then aligns the selected sequences using <a href="http://mafft.cbrc.jp/alignment/software/">MAFFT</a>. Once aligned, the set of virus sequences is further cleaned by removing insertions relative to a reference sequence to	enforce canonical site numbering, by removing sequences that show either too much or too little divergence relative to the expectation given their sampling date, and by removing known reassortant clusters. As reference for each viral lineage, we chose a well characterized virus without defining the canonical amino-acid numbering.
+						<h2>Alignment</h2>
+
+            Augur then aligns the selected sequences using <a href="http://mafft.cbrc.jp/alignment/software/">MAFFT</a>. Once aligned, the set of virus sequences is further cleaned by removing insertions relative to a reference sequence to enforce canonical site numbering and by removing sequences that show either too much or too little divergence relative to the expectation given their sampling date. As reference for each viral lineage, we chose a well characterized virus without defining the canonical amino-acid numbering.
 
 						<h2>Tree Building</h2>
 
-						From the filtered and cleaned alignment, <a href="https://github.com/nextstrain/augur">augur</a> builds a phylogenetic tree using <a href="http://www.microbesonline.org/fasttree/">FastTree</a>, which is then further refined using <a href="http://sco.h-its.org/exelixis/web/software/raxml/index.html">RAxML</a>. Next, augur uses <a href="https://github.com/neherlab/treetime">treetime</a> to infer a time tree based on the tree topology determined by RAxML and to infer nucleotide sequence and geographic location of every internal node of the tree. Missing sequence data at phylogeny tips is filled with the nearest ancestral sequence at these sites. The final tree is decorated with the attributes to be displayed in the browser.
+						From the filtered and cleaned alignment, we build a phylogenetic tree using <a href="http://www.microbesonline.org/fasttree/">FastTree</a>, which is then further refined using <a href="http://sco.h-its.org/exelixis/web/software/raxml/index.html">RAxML</a>. Next, augur uses <a href="https://github.com/neherlab/treetime">treetime</a> to infer a time tree based on the tree topology determined by RAxML and to infer nucleotide sequence and geographic location of every internal node of the tree. Missing sequence data at phylogeny tips is filled with the nearest ancestral sequence at these sites. The final tree is decorated with the attributes to be displayed in the browser.
 
           </div>
 
