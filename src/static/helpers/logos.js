97fdcbb9c888424dda2783cf110f4581b509428c
James Hadfield <jh22@sanger.ac.uk>
2017-03-01 12:10:59 -0800

    generateLogos moved to helper fn

---
+++ src/static/helpers/logos.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const generateLogos = [
+  <a key={1} href="http://www.fredhutch.org/" target="_blank">
+    <img width="75" src={require("../../images/fred-hutch-logo-small.png")}/>
+  </a>,
+  <a key={2} href="http://www.eb.tuebingen.mpg.de/" target="_blank">
+    <img width="65" src={require("../../images/max-planck-logo-small.png")}/>
+  </a>,
+  <a key={3} href="https://www.nih.gov/" target="_blank">
+    <img width="52" src={require("../../images/nih-logo-small.png")}/>
+  </a>,
+  <a key={4} href="https://erc.europa.eu/" target="_blank">
+    <img width="60" src={require("../../images/erc-logo-small.png")}/>
+  </a>,
+  <a key={5} href="https://www.openscienceprize.org/" target="_blank">
+    <img width="82" src={require("../../images/osp-logo-small.png")}/>
+  </a>,
+  <a key={6} href="http://biozentrum.org/" target="_blank">
+    <img width="85" src={require("../../images/bz_logo.png")}/>
+  </a>
+];
--- src/static/splash.js
+++ src/static/splash.js
@@ -8,7 +8,7 @@ import Title from "../components/framework/title";
 import { headerFont, materialButtonOutline } from "../globalStyles";
 import Flex from "../components/framework/flex";
 import { analyticsNewPage, triggerOutboundEvent } from "../util/googleAnalytics";
-// import Card from "./framework/card";
+import { generateLogos } from "./helpers/logos";
 
 const styles = {
   cardMainText: {
@@ -96,30 +96,6 @@ const generateCard = (title, imgRequired, to, outboundLink) => {
     );
   }
 };
-
-
-/* helper / generating functions */
-const generateLogos = [
-  <a key={1} href="http://www.fredhutch.org/" target="_blank">
-    <img width="75" src={require("../images/fred-hutch-logo-small.png")}/>
-  </a>,
-  <a key={2} href="http://www.eb.tuebingen.mpg.de/" target="_blank">
-    <img width="65" src={require("../images/max-planck-logo-small.png")}/>
-  </a>,
-  <a key={3} href="https://www.nih.gov/" target="_blank">
-    <img width="52" src={require("../images/nih-logo-small.png")}/>
-  </a>,
-  <a key={4} href="https://erc.europa.eu/" target="_blank">
-    <img width="60" src={require("../images/erc-logo-small.png")}/>
-  </a>,
-  <a key={5} href="https://www.openscienceprize.org/" target="_blank">
-    <img width="82" src={require("../images/osp-logo-small.png")}/>
-  </a>,
-  <a key={6} href="http://biozentrum.org/" target="_blank">
-    <img width="85" src={require("../images/bz_logo.png")}/>
-  </a>
-];
-
 
 class Splash extends React.Component {
   componentWillMount() {
