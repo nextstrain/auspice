56b8c259630f8da6a6b463d947af32694508c21e
sidneymbell <sidneymb@uw.edu>
2017-04-10 16:32:49 -0700

    Pseudocode to check the default geoResolution exists.

--- src/actions/loadData.js
+++ src/actions/loadData.js
@@ -2,6 +2,8 @@
 import { updateColorScale, updateNodeColors } from "./colors";
 import { dataURLStem } from "../util/globals";
 import * as types from "./types";
+import { CHANGE_GEO_RESOLUTION } from "./types";
+import {defaultGeoResolution} from "../util/globals";
 
 
 /* request metadata */
@@ -61,6 +63,24 @@ const populateMetadataStore = (queryParams) => {
   };
 };
 
+const checkGeoResolution = (node) => {
+  return {
+    if defaultGeoResolution not in node.attr():{
+    // need to fix conditional syntax.... I have no idea where the semicolons go ;)
+      if 'region' in node.attr():{ // default is 'country'; if null, try 'region'
+        type: CHANGE_GEO_RESOLUTION,
+        data: 'region'};
+      else:{
+        type: CHANGE_GEO_RESOLUTION,  // if neither 'region' nor 'country' available, don't draw the map.
+                                            // should also add a conditional to the map.js component checking that state.controls.georesolution exists
+        data: null};
+    };
+    // else:{
+      // do I need an else clause here to say "do nothing" if the default is fine?
+    // }
+  }
+}
+
 /* request tree */
 
 const requestTree = () => {
@@ -97,6 +117,8 @@ const populateTreeStore = (queryParams) => {
     return fetchTree(queryParams).then((res) => res.json()).then(
       (json) => {
         dispatch(receiveTree(json, controls));
+        // need to figure out how to access the first node in the tree object / where the tree object lives. Pass this as argument.
+        dispatch(checkGeoResolution(nodes[0]));
         dispatch(updateColorScale());
         dispatch(updateNodeColors());
       },
