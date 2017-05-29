ba8962029b524a1a62b44a8278f32371ef690502
Colin Megill <colinmegill@gmail.com>
2017-05-28 19:42:14 -0700

    validate map animation inputs

--- src/components/controls/map-animation.js
+++ src/components/controls/map-animation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SelectLabel from "../framework/select-label";
+import DatePicker from "react-datepicker";
 import _ from "lodash";
 import { select} from "../../globalStyles";
 import { connect } from "react-redux";
@@ -10,6 +11,103 @@ import { changeColorBy } from "../../actions/colors";
 import { modifyURLquery } from "../../util/urlHelpers";
 import { analyticsControlsEvent } from "../../util/googleAnalytics";
 import moment from 'moment';
+import { controlsWidth } from "../../util/globals";
+import { dataFont, darkGrey } from "../../globalStyles";
+
+moment.updateLocale("en", {
+  longDateFormat: {
+    L: "YYYY-MM-DD"
+  }
+});
+
+@connect((state) => {
+  return {
+    dateMin: state.controls.dateMin,
+    dateMax: state.controls.dateMax,
+    absoluteDateMin: state.controls.absoluteDateMin,
+    absoluteDateMax: state.controls.absoluteDateMax,
+    dateScale: state.controls.dateScale,
+    dateFormat: state.controls.dateFormat,
+    mapAnimationStartDate: state.controls.mapAnimationStartDate,
+  };
+})
+
+class MapAnimationStartDatePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lastSliderUpdateTime: Date.now()
+    }
+  }
+
+  numericToCalendar(numDate) {
+    return(this.props.dateFormat(this.props.dateScale.invert(numDate)));
+  }
+
+  calendarToNumeric(calDate) {
+    return(this.props.dateScale(this.props.dateFormat.parse(calDate)));
+  };
+
+  validateSelectedAnimationStartDate(input) {
+
+    /* duplicated */
+    let absoluteDateMinStr = this.props.absoluteDateMin;
+    let absoluteDateMaxStr = this.props.absoluteDateMax;
+    let absoluteDateMin = moment(absoluteDateMinStr, "YYYY-MM-DD");
+    let absoluteDateMax = moment(absoluteDateMaxStr, "YYYY-MM-DD");
+    // let numberDays = moment.duration(absoluteDateMax.diff(absoluteDateMin)).asDays();
+    // let maxDate = absoluteDateMax.diff(numberDays * 0.1, "days") // Latest start date is end date - 10%
+
+    let inputDate = moment(input, "YYYY-MM-DD")
+
+    let startDate;
+    if (inputDate <= absoluteDateMin){
+      startDate = absoluteDateMinStr;
+    } else if (inputDate >= absoluteDateMax){
+      startDate = absoluteDateMaxStr;
+    } else {
+      startDate = moment(input).format("YYYY-MM-DD")
+    }
+
+    return startDate;
+
+  }
+
+  updateFromPicker(userSelectedDateAsMoment) {
+
+      analyticsControlsEvent("change-animation-start-date");
+      this.props.dispatch({
+        type: CHANGE_ANIMATION_START,
+        data: this.validateSelectedAnimationStartDate(userSelectedDateAsMoment)
+      }); //might need to convert format
+  }
+
+  render() {
+    // minDistance={(this.props.absoluteMaxVal - this.props.absoluteMinVal) / 10.0}
+    // onChange={this.updateFromSlider.bind(this, true)}
+
+    // const absoluteMin = this.props.absoluteDateMin;
+    // const absoluteMax = this.props.absoluteDateMax;
+    // const selectedMin = this.props.dateMin;
+    // const selectedMax = this.props.dateMax;
+    //
+    // const absoluteMinNumDate = this.calendarToNumeric(absoluteMin);
+    // const absoluteMaxNumDate = this.calendarToNumeric(absoluteMax);
+    // const selectedMinNumDate = this.calendarToNumeric(selectedMin);
+    // const selectedMaxNumDate = this.calendarToNumeric(selectedMax);
+
+    return (
+      <div style={{marginBottom: 15, width: controlsWidth}}>
+        <DatePicker
+          dateFormat="YYYY/MM/DD"
+
+          selected={moment(this.props.mapAnimationStartDate)}
+          onChange={this.updateFromPicker.bind(this)}
+        />
+      </div>
+    );
+  }
+}
 
 @connect((state) => {
   return {
@@ -19,66 +117,26 @@ import moment from 'moment';
     mapAnimationPathTrailing: state.controls.mapAnimationPathTrailing
   };
 })
-
 class MapAnimationControls extends React.Component {
 
-  // static contextTypes = {
-    // router: React.PropTypes.object.isRequired
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  }
+
+  // checkAndTransformAnimationDuration(input) {
+  //
+  //   const minimumMapAnimationTimeInSeconds = 5;
+  //   const maximumMapAnimationTimeInSeconds = 120;
+  //   let validatedInput = input;
+  //
+  //   if (input < minimumMapAnimationTimeInSeconds) {
+  //     validatedInput = minimumMapAnimationTimeInSeconds;
+  //   } else if (input > maximumMapAnimationTimeInSeconds) {
+  //     validatedInput = maximumMapAnimationTimeInSeconds;
+  //   }
+  //
+  //   return validatedInput * 1000 // ship milliseconds instead of seconds to reducer
   // }
-
-
-
-  checkAndTransformAnimationTime(input) {
-
-    /* duplicated */
-    let absoluteDateMinStr = this.props.absoluteDateMin;
-    let absoluteDateMaxStr = this.props.absoluteDateMax;
-    let absoluteDateMin = moment(absoluteDateMinStr, 'YYYY-MM-DD');
-    let absoluteDateMax = moment(absoluteDateMaxStr, 'YYYY-MM-DD');
-    let numberDays = moment.duration(absoluteDateMax.diff(absoluteDateMin)).asDays();
-
-    //input is seconds
-    let max_seconds = numberDays;
-    let min_seconds= 5;
-
-    // _.isNumber validation
-    let animationLength
-    if (input <= min_seconds){
-      animationLength = min_seconds
-    }
-    else if (input >= max_seconds){
-      animationLength = max_seconds
-    }
-    else{
-      animationLength = input
-    }
-    return animationLength*1000 //return milliseconds instead of seconds
-  }
-
-  checkAnimationDate(input) {
-
-    /* duplicated */
-    let absoluteDateMinStr = this.props.absoluteDateMin;
-    let absoluteDateMaxStr = this.props.absoluteDateMax;
-    let absoluteDateMin = moment(absoluteDateMinStr, 'YYYY-MM-DD');
-    let absoluteDateMax = moment(absoluteDateMaxStr, 'YYYY-MM-DD');
-    let numberDays = moment.duration(absoluteDateMax.diff(absoluteDateMin)).asDays();
-
-
-    let maxDate = absoluteDateMax.diff(numberDays*0.1, 'days') // Latest start date is end date - 10%
-    let inputDate = moment(input, 'YYYY-MM-DD')
-
-    let startDate
-    if (inputDate <= absoluteDateMin){
-      startDate = absoluteDateMinStr;
-    } else if (inputdate >= maxDate){
-      startDate = maxDate.year(year).month(month).date(day);
-    } else{
-      startDate = input;
-    };
-    return startDate;
-
-  }
 
   render() {
 
