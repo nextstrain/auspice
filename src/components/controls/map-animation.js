0269dbea69be8211591cb3bae8badaf31d8c46df
Colin Megill <colinmegill@gmail.com>
2017-05-28 19:40:10 -0700

    Add map animation control inputs & events

--- src/components/controls/map-animation.js
+++ src/components/controls/map-animation.js
@@ -91,37 +91,44 @@ class MapAnimationControls extends React.Component {
 
     return (
       <div id='mapAnimationControls'>
-      <SelectLabel text="Animation length (seconds)"/>
-      <SelectLabel text="Animation path trailing"/>
-      <SelectLabel text="Animation start date (YYYY-MM-DD)"/>
 
-      <input
-        type='date'
-        id='animationStartDate'
-        value={this.props.mapAnimationStartDate}
+        <SelectLabel text="Animation length (seconds)"/>
+        <input
+          style={{
+            padding: "5px 10px",
+            borderRadius: 3,
+            border: "1px solid lightgrey",
+            marginBottom: 10,
+            fontSize: 14,
+          }}
+          type='text'
+          id='animationLengthInSeconds'
+          value={this.props.mapAnimationDurationInMilliseconds / 1000 /* we get milliseconds from reducer, and send milliseconds as well, see: checkAndTransformAnimationDuration */}
+          onChange={(e) => {
+            analyticsControlsEvent("change-animation-time");
+            /* cast string to num, the see if its an integer, ie., don't send the action if they type 'd' */
+            if (Number.isInteger(+e.target.value)) {
+              this.props.dispatch({
+                type: CHANGE_ANIMATION_TIME,
+                data: +e.target.value * 1000 /* this.checkAndTransformAnimationDuration(+e.target.value) */
+              });
+            }
+          }}/>
 
-        onChange={(e) => {
-          analyticsControlsEvent("change-animation-start");
-          this.props.dispatch({ type: CHANGE_ANIMATION_START, data: checkAnimationTime(e.target.value) }); //might need to convert format
-          // modifyURLquery(this.context.router, {as: e.target.value}, true);
-        }}/>
-      <input
-        type='text'
-        id='animationLengthInSeconds'
-        value={this.props.mapAnimationLengthInSeconds}
-        onChange={(e) => {
-          analyticsControlsEvent("change-animation-time");
-          this.props.dispatch({ type: CHANGE_ANIMATION_TIME, data: checkInput(e.target.value) });
-          // modifyURLquery(this.context.router, {at: e.target.value}, true);
-        }}/>
-      <input
-        type='checkbox'
-        id='mapAnimationPathTrailing'
-        onChange={(e) => {
-          analyticsControlsEvent("change-animation-pathtrailing");
-          this.props.dispatch({ type: CHANGE_ANIMATION_PATHTRAILING, data: checkInput(e.target.value) });
-          // modifyURLquery(this.context.router, {apt: e.target.value}, true);
-        }}/>
+        <SelectLabel text="Animation start date (click to change)"/>
+        <MapAnimationStartDatePicker/>
+
+        <SelectLabel text="Animation path trailing"/>
+        <input
+          type='checkbox'
+          checked={this.props.mapAnimationPathTrailing}
+          id='mapAnimationPathTrailing'
+          onChange={(e) => {
+            analyticsControlsEvent("change-animation-pathtrailing");
+            this.props.dispatch({ type: CHANGE_ANIMATION_PATHTRAILING, data: !this.props.mapAnimationPathTrailing });
+            // modifyURLquery(this.context.router, {apt: e.target.value}, true);
+          }}/>
+
         </div>
     );
   }
