31516f224392881825d5b9382b719a4f7cf4ec90
Trevor Bedford <trevor@bedford.io>
2017-07-07 16:56:29 -0700

    Restyle animation speed button and decruft animation controls
    Fixes #370

--- src/components/controls/map-animation.js
+++ src/components/controls/map-animation.js
@@ -1,114 +1,13 @@
 import React from "react";
 import SelectLabel from "../framework/select-label";
-import DatePicker from "react-datepicker";
-import _ from "lodash";
-import { select} from "../../globalStyles";
 import { connect } from "react-redux";
 import { CHANGE_ANIMATION_TIME } from "../../actions/types";//*
 import { CHANGE_ANIMATION_START } from "../../actions/types"; //*
 import { CHANGE_ANIMATION_CUMULATIVE } from "../../actions/types"; //*
-import { changeColorBy } from "../../actions/colors";
 import { modifyURLquery } from "../../util/urlHelpers";
 import { analyticsControlsEvent } from "../../util/googleAnalytics";
-import moment from 'moment';
-import { controlsWidth } from "../../util/globals";
-import { dataFont, darkGrey } from "../../globalStyles";
 import Toggle from "./toggle";
-
-moment.updateLocale("en", {
-  longDateFormat: {
-    L: "YYYY-MM-DD"
-  }
-});
-
-@connect((state) => {
-  return {
-    dateMin: state.controls.dateMin,
-    dateMax: state.controls.dateMax,
-    absoluteDateMin: state.controls.absoluteDateMin,
-    absoluteDateMax: state.controls.absoluteDateMax,
-    dateScale: state.controls.dateScale,
-    dateFormat: state.controls.dateFormat,
-    // mapAnimationStartDate: state.controls.mapAnimationStartDate,
-  };
-})
-
-class MapAnimationStartDatePicker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lastSliderUpdateTime: Date.now()
-    }
-  }
-
-  numericToCalendar(numDate) {
-    return(this.props.dateFormat(this.props.dateScale.invert(numDate)));
-  }
-
-  calendarToNumeric(calDate) {
-    return(this.props.dateScale(this.props.dateFormat.parse(calDate)));
-  };
-
-  validateSelectedAnimationStartDate(input) {
-
-    /* duplicated */
-    let absoluteDateMinStr = this.props.absoluteDateMin;
-    let absoluteDateMaxStr = this.props.absoluteDateMax;
-    let absoluteDateMin = moment(absoluteDateMinStr, "YYYY-MM-DD");
-    let absoluteDateMax = moment(absoluteDateMaxStr, "YYYY-MM-DD");
-    // let numberDays = moment.duration(absoluteDateMax.diff(absoluteDateMin)).asDays();
-    // let maxDate = absoluteDateMax.diff(numberDays * 0.1, "days") // Latest start date is end date - 10%
-
-    let inputDate = moment(input, "YYYY-MM-DD")
-
-    let startDate;
-    if (inputDate <= absoluteDateMin){
-      startDate = absoluteDateMinStr;
-    } else if (inputDate >= absoluteDateMax){
-      startDate = absoluteDateMaxStr;
-    } else {
-      startDate = moment(input).format("YYYY-MM-DD")
-    }
-
-    return startDate;
-
-  }
-
-  updateFromPicker(userSelectedDateAsMoment) {
-
-      analyticsControlsEvent("change-animation-start-date");
-      this.props.dispatch({
-        type: CHANGE_ANIMATION_START,
-        data: this.validateSelectedAnimationStartDate(userSelectedDateAsMoment)
-      }); //might need to convert format
-  }
-
-  render() {
-    // minDistance={(this.props.absoluteMaxVal - this.props.absoluteMinVal) / 10.0}
-    // onChange={this.updateFromSlider.bind(this, true)}
-
-    // const absoluteMin = this.props.absoluteDateMin;
-    // const absoluteMax = this.props.absoluteDateMax;
-    // const selectedMin = this.props.dateMin;
-    // const selectedMax = this.props.dateMax;
-    //
-    // const absoluteMinNumDate = this.calendarToNumeric(absoluteMin);
-    // const absoluteMaxNumDate = this.calendarToNumeric(absoluteMax);
-    // const selectedMinNumDate = this.calendarToNumeric(selectedMin);
-    // const selectedMaxNumDate = this.calendarToNumeric(selectedMax);
-
-    return (
-      <div style={{marginBottom: 15, width: controlsWidth}}>
-        <DatePicker
-          dateFormat="YYYY/MM/DD"
-
-          selected={moment(this.props.mapAnimationStartDate)}
-          onChange={this.updateFromPicker.bind(this)}
-        />
-      </div>
-    );
-  }
-}
+import {materialButton, materialButtonSelected} from "../../globalStyles";
 
 @connect((state) => {
   return {
@@ -169,63 +68,27 @@ class MapAnimationControls extends React.Component {
 
   render() {
 
-    /* duplicated */
-    let absoluteDateMinStr = this.props.absoluteDateMin;
-    let absoluteDateMaxStr = this.props.absoluteDateMax;
-    let absoluteDateMin = moment(absoluteDateMinStr, 'YYYY-MM-DD');
-    let absoluteDateMax = moment(absoluteDateMaxStr, 'YYYY-MM-DD');
-    let numberDays = moment.duration(absoluteDateMax.diff(absoluteDateMin)).asDays();
-
     return (
       <div id='mapAnimationControls'>
 
       <div style={{marginBottom: 15}}>
         <SelectLabel text="Animation speed"/>
         <button
-          style={{
-            padding: "5px 10px",
-            outline: "none",
-            color: this.props.mapAnimationDurationInMilliseconds === 60000 ? "white" : "black",
-            backgroundColor: this.props.mapAnimationDurationInMilliseconds === 60000 ? "rgb(80, 151, 186)" : "white",
-            border: this.props.mapAnimationDurationInMilliseconds === 60000 ? "1px solid rgb(80, 151, 186)" : "1px solid lightgrey",
-            marginRight: 10,
-            borderRadius: 3,
-            fontSize: 14,
-          }}
+          style={this.props.mapAnimationDurationInMilliseconds === 60000 ? materialButtonSelected : materialButton}
           onClick={this.handleChangeAnimationTimeClicked("slow")}>
           Slow
         </button>
         <button
-          style={{
-            padding: "5px 10px",
-            outline: "none",
-            color: this.props.mapAnimationDurationInMilliseconds === 30000 ? "white" : "black",
-            backgroundColor: this.props.mapAnimationDurationInMilliseconds === 30000 ? "rgb(80, 151, 186)" : "white",
-            border: this.props.mapAnimationDurationInMilliseconds === 30000 ? "1px solid rgb(80, 151, 186)" : "1px solid lightgrey",
-            marginRight: 10,
-            borderRadius: 3,
-            fontSize: 14,
-          }}
+          style={this.props.mapAnimationDurationInMilliseconds === 30000 ? materialButtonSelected : materialButton}
           onClick={this.handleChangeAnimationTimeClicked("medium")}>
           Medium
         </button>
         <button
-          style={{
-            padding: "5px 10px",
-            outline: "none",
-            color: this.props.mapAnimationDurationInMilliseconds === 15000 ? "white" : "black",
-            backgroundColor: this.props.mapAnimationDurationInMilliseconds === 15000 ? "rgb(80, 151, 186)" : "white",
-            border: this.props.mapAnimationDurationInMilliseconds === 15000 ? "1px solid rgb(80, 151, 186)" : "1px solid lightgrey",
-            marginRight: 10,
-            borderRadius: 3,
-            fontSize: 14,
-          }}
+          style={this.props.mapAnimationDurationInMilliseconds === 15000 ? materialButtonSelected : materialButton}
           onClick={this.handleChangeAnimationTimeClicked("fast")}>
           Fast
         </button>
       </div>
-      {/*<SelectLabel text="Animation start date (click to change)"/>*/}
-      {/*<MapAnimationStartDatePicker/>*/}
 
       <Toggle
         display={true}
@@ -243,7 +106,3 @@ class MapAnimationControls extends React.Component {
 }
 
 export default MapAnimationControls
-
-
-// max={absoluteDateMax.year().month().date()} //These probably need to be reformatted (or just kept as strings above)
-// min={absoluteDateMin.year().month().date()}
