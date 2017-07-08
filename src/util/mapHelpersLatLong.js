e3ac4ed544e3e0fd57b51b88b11e36a370a229da
Trevor Bedford <trevor@bedford.io>
2017-07-08 15:52:10 -0700

    Cleanup code to shelve out a maybeGetTransmissionPair function

--- src/util/mapHelpersLatLong.js
+++ src/util/mapHelpersLatLong.js
@@ -1,15 +1,41 @@
 import {averageColors} from "./colorHelpers";
 import {getTipColorAttribute} from "./treeHelpers";
 
+// longs of original map are -180 to 180
+// longs of fully triplicated map are -540 to 540
+// restrict to longs between -360 to 360
+const westBound = -360;
+const eastBound = 360;
+
 /* interchange. this is a leaflet method that will tell d3 where to draw.
 -Note (A) we MAY have to do this every time rather than just once */
 const leafletLatLongToLayerPoint = (lat, long, map) => {
   return map.latLngToLayerPoint(new L.LatLng(lat, long))
 }
 
+/* if transmission pair is legal, return a leaflet LatLng origin / dest pair
+otherwise return null */
+const maybeGetTransmissionPair = (latOrig, longOrig, latDest, longDest, map) => {
+
+  // if either origin or destination are inside bounds, include
+  // transmission must be less than 180 lat difference
+  if (
+    (longOrig > westBound || longDest > westBound) &&
+    (longOrig < eastBound || longDest < eastBound) &&
+    (Math.abs(longOrig - longDest) < 180)
+  ) {
+    return [
+      leafletLatLongToLayerPoint(latOrig, longOrig, map),
+      leafletLatLongToLayerPoint(latDest, longDest, map)
+    ];
+  }
+  else {
+    return null;
+  }
+}
+
 const aggregated = (nodes, visibility, geoResolution, nodeColors) => {
   const aggregatedLocations = {}; /* demes */
-  const aggregatedLocationsWraparoundCopy = {}; /* edges, animation paths */
   const aggregatedTransmissions = {}; /* edges, animation paths */
 
   /*
@@ -17,7 +43,7 @@ const aggregated = (nodes, visibility, geoResolution, nodeColors) => {
     can count how many times observe a tip and make an array of an attribute,
     which in this case will be color,
     could be an array of hexes which would be enough,
-    similar operation for each transmissino -
+    similar operation for each transmission -
     for each deme pair observe how many and record a hex value from the from deme and
     count them for width and average to get color
   */
@@ -30,7 +56,6 @@ const aggregated = (nodes, visibility, geoResolution, nodeColors) => {
     if (!n.children) {
       if (!aggregatedLocations[n.attr[geoResolution]]) {
         aggregatedLocations[n.attr[geoResolution]] = [];
-        aggregatedLocationsWraparoundCopy[n.attr[geoResolution]] = [];
       }
     }
     if (n.children) {
@@ -76,7 +101,6 @@ const aggregated = (nodes, visibility, geoResolution, nodeColors) => {
   });
   return {
     aggregatedLocations,
-    aggregatedLocationsWraparoundCopy,
     aggregatedTransmissions
   }
 }
@@ -84,12 +108,6 @@ const aggregated = (nodes, visibility, geoResolution, nodeColors) => {
 export const getLatLongs = (nodes, visibility, metadata, map, geoResolution, triplicate, nodeColors) => {
 
   let offsets = triplicate ? [-360, 0, 360] : [0]
-
-  // longs of original map are -180 to 180
-  // longs of fully triplicated map are -540 to 540
-  // restrict to longs between -360 to 360
-  const westBound = -360;
-  const eastBound = 360;
 
   const {
     aggregatedLocations,
@@ -117,90 +135,44 @@ export const getLatLongs = (nodes, visibility, metadata, map, geoResolution, tri
     });
   })
 
+  // offset is applied to transmission origin
+  offsets.forEach((offsetOrig) => {
 
-  offsets.forEach((OFFSET) => {
-
-    /* count TRANSMISSIONS for line thickness */
     _.forOwn(aggregatedTransmissions, (value, key) => {
 
       const locs = key.split("@")[0].split("|");
 
-      // /* we already know the path for china to us because we've already done us to china */
-      // if (
-      //   knownShortestPaths[countries[0] + "/" + countries[1]] ||
-      //   knownShortestPaths[countries[1] + "/" + countries[0]]
-      // ) {
-      //   /* use the one we already know about from knownShortestPaths. */
-      // } else {
-      //   /* figure out the new one, insert it AND ITS REVERSE into the knownShortestPaths map above */
-      // }
-
-      let longOrig;
-      let longDest;
-      let latOrig;
-      let latDest;
+      let latOrig, longOrig, latDest, longDest;
       try {
-        longOrig = geo[geoResolution][locs[0]].longitude;
-        longDest = geo[geoResolution][locs[1]].longitude;
         latOrig = geo[geoResolution][locs[0]].latitude;
+        longOrig = geo[geoResolution][locs[0]].longitude;
         latDest = geo[geoResolution][locs[1]].latitude;
+        longDest = geo[geoResolution][locs[1]].longitude;
       } catch (e) {
         // console.log("No transmission lat/longs for ", countries[0], " -> ", countries[1])
         return;
       }
 
+      // origin to destination
       let pairs = [];
 
-      // if either origin or destination are inside bounds, include
-      // transmission must be less than 180 lat difference
-      if (
-        (longOrig + OFFSET > westBound || longDest > westBound) &&
-        (longOrig + OFFSET < eastBound || longDest < eastBound) &&
-        (Math.abs( (longOrig + OFFSET) - (longDest) ) < 180)
-      ) {
-        const middleDest = [
-          leafletLatLongToLayerPoint(latOrig, longOrig + OFFSET, map),
-          leafletLatLongToLayerPoint(latDest, longDest, map)
-        ];
-        pairs.push(middleDest);
-      }
-
-      // if either origin or destination are inside bounds, include
-      // transmission must be less than 180 lat difference
-      if (
-        (longOrig + OFFSET > westBound || longDest - 360 > westBound) &&
-        (longOrig + OFFSET < eastBound || longDest - 360 < eastBound) &&
-        (Math.abs( (longOrig + OFFSET) - (longDest - 360) ) < 180)
-      ) {
-        const westDest = [
-          leafletLatLongToLayerPoint(latOrig, longOrig + OFFSET, map),
-          leafletLatLongToLayerPoint(latDest, longDest - 360, map)
-        ];
-        pairs.push(westDest);
-      }
-
-      // if either origin or destination are inside bounds, include
-      // transmission must be less than 180 lat difference
-      if (
-        (longOrig + OFFSET > westBound || longDest + 360 > westBound) &&
-        (longOrig + OFFSET < eastBound || longDest + 360 < eastBound) &&
-        (Math.abs( (longOrig + OFFSET) - (longDest + 360) ) < 180)
-      ) {
-        const eastDest = [
-          leafletLatLongToLayerPoint(latOrig, longOrig + OFFSET, map),
-          leafletLatLongToLayerPoint(latDest, longDest + 360, map)
-        ];
-        pairs.push(eastDest);
-      }
+      // iterate over offsets applied to transmission destination
+      // even if map is not tripled
+      [-360, 0, 360].forEach((offsetDest) => {
+        const pair = maybeGetTransmissionPair(latOrig, longOrig + offsetOrig, latDest, longDest + offsetDest, map);
+        if (pair) {
+          pairs.push(pair);
+        }
+      })
 
       if (pairs.length === 0) { return; }
 
       /* this gives us an index for both demes in the transmission pair with which we will access the node array */
-      const winningPair = _.minBy(pairs, (pair) => { return Math.abs(pair[1].x - pair[0].x) });
+      const closestPair = _.minBy(pairs, (pair) => { return Math.abs(pair[1].x - pair[0].x) });
 
       const transmission = {
         demePairIndices: key.split("@")[2].split("|"), /* this has some weird values occassionally that do not presently break anything. created/discovered during animation work. */
-        originToDestinationXYs: winningPair,
+        originToDestinationXYs: closestPair,
         total: value.length, /* changes over time */
         color: averageColors(value), /* changes over time */
       }
@@ -208,7 +180,7 @@ export const getLatLongs = (nodes, visibility, metadata, map, geoResolution, tri
       demesAndTransmissions.transmissions.push({data: transmission})
     });
 
-  }) /* End OFFSET */
+  }) /* End offsetOrig */
 
   // console.log("This is a transmission from mapHelpersLatLong.js: ", demesAndTransmissions)
   if (demesAndTransmissions.transmissions.length) {
@@ -220,19 +192,3 @@ export const getLatLongs = (nodes, visibility, metadata, map, geoResolution, tri
   }
   return demesAndTransmissions;
 }
-
-/* we used to return this in transmission */
-// create new leaflet LatLong objects
-// const start = new L.LatLng(lat0, long0)
-// const end = new L.LatLng(lat1, long1)
-// start, /* changes with zoom level */
-// end, /* changes with zoom level */
-// from: countries[0],
-// to: countries[1]
-
-/* we used to compute this... likely with geodesic from leaflet */
-// const dlambda = Math.abs((long1-long0)%360)*Math.PI/180.0;
-// const angle = Math.acos(Math.sin(lat1*Math.PI/180.0)*Math.sin(lat0*Math.PI/180.0)
-//                         + Math.cos(lat1*Math.PI/180.0)*Math.cos(lat0*Math.PI/180.0)*Math.cos(dlambda));
-// // this sets the number of segments of the path, min 4, max 36 for half way around the globe
-// const nSteps = Math.ceil(Math.max(4,angle*36/Math.PI));
