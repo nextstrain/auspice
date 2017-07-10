afd6bafde89c655e8041ce026789edc81859936c
Trevor Bedford <trevor@bedford.io>
2017-07-10 13:42:24 -0700

    Small fix to allow full zooming

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -341,7 +341,7 @@ class Map extends React.Component {
       scrollWheelZoom: false,
       maxBounds: this.getBounds(),
       minZoom: 2,
-      maxZoom: 8,
+      maxZoom: 10,
       zoomControl: false,
       /* leaflet sleep see https://cliffcloud.github.io/Leaflet.Sleep/#summary */
       // true by default, false if you want a wild map
