f284d661115b2f0f1b218539b7f25718f87d1f1d
James Hadfield <jh22@sanger.ac.uk>
2017-07-11 14:41:45 -0700

    filters header prototype

--- src/components/app.js
+++ src/components/app.js
@@ -15,6 +15,7 @@ import * as globals from "../util/globals";
 import Sidebar from "react-sidebar";
 import TitleBar from "./framework/title-bar";
 import Footer from "./framework/footer";
+import FilterOnHeader from "./framework/filterOnHeader";
 import { analyticsNewPage } from "../util/googleAnalytics";
 
 /* BRIEF REMINDER OF PROPS AVAILABLE TO APP:
@@ -83,6 +84,7 @@ class App extends React.Component {
               handler={() => {this.setState({sidebarDocked: !this.state.sidebarDocked});}}
             />
           }
+          <FilterOnHeader sidebar={this.state.sidebarOpen || this.state.sidebarDocked}/>
           <TreeView
             query={queryString.parse(this.context.router.history.location.search)}
             sidebar={this.state.sidebarOpen || this.state.sidebarDocked}
