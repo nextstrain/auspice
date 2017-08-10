e20e706b44611c1f5220f0d83e0bad52354af608
James Hadfield <jh22@sanger.ac.uk>
2017-08-10 16:46:25 -0700

    modal filled out

--- src/components/controls/download.js
+++ src/components/controls/download.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 import { DISMISS_DOWNLOAD_MODAL } from "../../actions/types";
-import { materialButton } from "../../globalStyles";
+import { materialButton, medGrey } from "../../globalStyles";
+import RectangularTreeLayout from "../framework/svg-tree-layout-rectangular";
+
 
 @connect((state) => ({
   browserDimensions: state.browserDimensions.browserDimensions,
-  show: state.controls.showDownload
+  show: state.controls.showDownload,
+  metadata: state.metadata
 }))
 class DownloadModal extends React.Component {
   static propTypes = {
@@ -13,34 +16,80 @@ class DownloadModal extends React.Component {
     dispatch: React.PropTypes.func,
     browserDimensions: React.PropTypes.object
   }
-  getStyles() {
+  getStyles(bw, bh) {
     return {
       behind: { /* covers the screen */
         position: "absolute",
-        width: this.props.browserDimensions.width,
-        height: this.props.browserDimensions.height,
+        width: bw,
+        height: bh,
         zIndex: 10000,
         backgroundColor: "rgba(0, 0, 0, 0.3)"
       },
       modal: {
-        marginLeft: 300,
-        marginTop: 200,
-        width: 500,
-        height: 500,
+        marginLeft: 200,
+        marginTop: 130,
+        width: bw - 2 * 200,
+        height: bh - 2 * 130,
         borderRadius: 2,
         backgroundColor: "rgba(250, 250, 250, 1)"
       }
     };
   }
+  downloadButtons() {
+    const names = ["Tree (newick)", "Metadata (CSV)"];
+    const callbacks = [
+      () => {console.log("make newick tree!");},
+      () => {console.log("download metadata!");}
+    ];
+    return (
+      <div className="row">
+        {names.map((name, idx) => (
+          <div key={idx} className="col-md-5">
+            <RectangularTreeLayout width={25} stroke={medGrey}/>
+            <button style={materialButton} onClick={callbacks[idx]}>
+              {name}
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   render() {
     if (!this.props.show) {
       return null;
     }
-    const styles = this.getStyles();
+    const styles = this.getStyles(this.props.browserDimensions.width, this.props.browserDimensions.height);
+    const meta = this.props.metadata.metadata;
     return (
       <div style={styles.behind}>
-        <div style={styles.modal}>
-          <h1>"DOWNLOAD MODAL"</h1>
+        <div className="static container" style={styles.modal}>
+          <div className="row">
+            <div className="col-md-1"/>
+            <div className="col-md-10">
+              <h1>Download Data</h1>
+              <h2>Dataset details</h2>
+              {meta.title} (last updated {meta.updated}) contains {meta.virus_count} sequences from {Object.keys(meta.author_info).length} authors, some of which may be unpublished.
+              <br/>
+
+              <h2>Data usage policy</h2>
+
+
+              <h2>Relevent publications (??)</h2>
+              <ul>
+                <li>Nextstrain paper (to write!)</li>
+                <li>TimeTree?</li>
+                <li>Titer model paper?</li>
+                <li>Dataset specific papers?</li>
+              </ul>
+
+              <h2>Download data as</h2>
+              {this.downloadButtons()}
+
+            </div>
+          </div>
+
+
           <button
             key={2}
             style={materialButton}
