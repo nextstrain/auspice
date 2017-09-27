3a15187ce90d08625de9caebe3d953fc1b31540b
Trevor Bedford <trevor@bedford.io>
2017-09-27 11:46:24 -0700

    Update get_data script with static files

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,12 +1,9 @@
 rm -rf data/
 mkdir -p data/
 
-datasets=( "manifest_guest.json" "manifest_mumps.json"
-  "img_ebola.png"\
+data_files=( "manifest_guest.json" "manifest_mumps.json"
   "ebola_tree.json" "ebola_sequences.json" "ebola_meta.json" "ebola_entropy.json"\
-  "img_zika.png"\
   "zika_tree.json" "zika_sequences.json" "zika_meta.json" "zika_entropy.json"\
-  "img_seasonalinfluenza.png"\
   "flu_h3n2_ha_3y_tree.json" "flu_h3n2_ha_3y_sequences.json" "flu_h3n2_ha_3y_meta.json" "flu_h3n2_ha_3y_entropy.json"\
   "flu_h3n2_ha_6y_tree.json" "flu_h3n2_ha_6y_sequences.json" "flu_h3n2_ha_6y_meta.json" "flu_h3n2_ha_6y_entropy.json"\
   "flu_h3n2_ha_12y_tree.json" "flu_h3n2_ha_12y_sequences.json" "flu_h3n2_ha_12y_meta.json" "flu_h3n2_ha_12y_entropy.json"\
@@ -19,7 +16,6 @@ datasets=( "manifest_guest.json" "manifest_mumps.json"
   "flu_yam_ha_3y_tree.json" "flu_yam_ha_3y_sequences.json" "flu_yam_ha_3y_meta.json" "flu_yam_ha_3y_entropy.json"\
   "flu_yam_ha_6y_tree.json" "flu_yam_ha_6y_sequences.json" "flu_yam_ha_6y_meta.json" "flu_yam_ha_6y_entropy.json"\
   "flu_yam_ha_12y_tree.json" "flu_yam_ha_12y_sequences.json" "flu_yam_ha_12y_meta.json" "flu_yam_ha_12y_entropy.json"\
-  "img_avianinfluenza.png"\
   "avian_h7n9_ha_tree.json" "avian_h7n9_ha_sequences.json" "avian_h7n9_ha_meta.json" "avian_h7n9_ha_entropy.json"\
   "avian_h7n9_mp_tree.json" "avian_h7n9_mp_sequences.json" "avian_h7n9_mp_meta.json" "avian_h7n9_mp_entropy.json"\
   "avian_h7n9_na_tree.json" "avian_h7n9_na_sequences.json" "avian_h7n9_na_meta.json" "avian_h7n9_na_entropy.json"\
@@ -28,16 +24,23 @@ datasets=( "manifest_guest.json" "manifest_mumps.json"
   "avian_h7n9_pa_tree.json" "avian_h7n9_pa_sequences.json" "avian_h7n9_pa_meta.json" "avian_h7n9_pa_entropy.json"\
   "avian_h7n9_pb1_tree.json" "avian_h7n9_pb1_sequences.json" "avian_h7n9_pb1_meta.json" "avian_h7n9_pb1_entropy.json"\
   "avian_h7n9_pb2_tree.json" "avian_h7n9_pb2_sequences.json" "avian_h7n9_pb2_meta.json" "avian_h7n9_pb2_entropy.json"\
-  "img_dengue.png"\
   "dengue_all_tree.json" "dengue_all_sequences.json" "dengue_all_meta.json" "dengue_all_entropy.json"\
   "dengue_denv1_tree.json" "dengue_denv1_sequences.json" "dengue_denv1_meta.json" "dengue_denv1_entropy.json"\
   "dengue_denv2_tree.json" "dengue_denv2_sequences.json" "dengue_denv2_meta.json" "dengue_denv2_entropy.json"\
   "dengue_denv3_tree.json" "dengue_denv3_sequences.json" "dengue_denv3_meta.json" "dengue_denv3_entropy.json"\
   "dengue_denv4_tree.json" "dengue_denv4_sequences.json" "dengue_denv4_meta.json" "dengue_denv4_entropy.json"\
-  "img_mumps.jpg"\
   "mumps_global_tree.json" "mumps_global_sequences.json" "mumps_global_meta.json" "mumps_global_entropy.json"\
   "mumps_bc_tree.json" "mumps_bc_sequences.json" "mumps_bc_meta.json" "mumps_bc_entropy.json"\
   "mumps_mass_tree.json" "mumps_mass_sequences.json" "mumps_mass_meta.json" "mumps_mass_entropy.json"\
+)
+
+static_files=(
+  "img_mumps.jpg"\
+  "img_dengue.png"\
+  "img_avianinfluenza.png"\
+  "img_seasonalinfluenza.png"\
+  "img_ebola.png"\
+  "img_zika.png"\  
   "figures_feb-2016_h1n1pdm_6b2_tree.png"\
   "figures_feb-2016_h1n1pdm_clades.png"\
   "figures_feb-2016_h1n1pdm_counts.png"\
@@ -130,7 +133,12 @@ datasets=( "manifest_guest.json" "manifest_mumps.json"
   "post_2017-02-23-feb-2017.md"\
   )
 
-for i in "${datasets[@]}"
+for i in "${data_files[@]}"
 do
   curl http://data.nextstrain.org/${i} --compressed -o data/${i}
+done
+
+for i in "${static_files[@]}"
+do
+  curl http://cdn.rawgit.com/nextstrain/themis/master/files/${i} --compressed -o static/${i}
 done
