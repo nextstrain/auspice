b406f04a04b978edbc8cdbdb58d8eb16fa540787
Trevor Bedford <trevor@bedford.io>
2017-09-27 08:44:36 -0700

    Fix typos in get_data shell script

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -61,20 +61,20 @@ datasets=( "manifest_guest.json" "manifest_mumps.json"
   "figures_feb-2017_h1n1pdm_frequencies.png"\
   "figures_feb-2017_h1n1pdm_mutations.png"\
   "figures_feb-2017_h1n1pdm_tree.png"\
-  "figures_feb-2017_h1n1pdm_tree.svg
+  "figures_feb-2017_h1n1pdm_tree.svg"\
   "figures_feb-2017_h3n2_clades.png"\
   "figures_feb-2017_h3n2_counts.png"\
   "figures_feb-2017_h3n2_frequencies.png"\
   "figures_feb-2017_h3n2_lbi.png"\
   "figures_feb-2017_h3n2_mutations.png"\
   "figures_feb-2017_h3n2_tree.png"\
-  "figures_feb-2017_h3n2_tree.svg
+  "figures_feb-2017_h3n2_tree.svg"\
   "figures_feb-2017_h3n2_tree_titer_model.png"\
-  "figures_feb-2017_h3n2_tree_titer_model.svg
+  "figures_feb-2017_h3n2_tree_titer_model.svg"\
   "figures_feb-2017_vic_counts.png"\
   "figures_feb-2017_vic_frequencies.png"\
   "figures_feb-2017_vic_lbi.png"\
-  "figures_feb-2017_vic_lbi.svg
+  "figures_feb-2017_vic_lbi.svg"\
   "figures_feb-2017_vic_mutations.png"\
   "figures_feb-2017_yam_clades.png"\
   "figures_feb-2017_yam_counts.png"\
@@ -82,7 +82,7 @@ datasets=( "manifest_guest.json" "manifest_mumps.json"
   "figures_feb-2017_yam_lbi.png"\
   "figures_feb-2017_yam_mutations.png"\
   "figures_feb-2017_yam_tree.png"\
-  "figures_feb-2017_yam_tree.svg
+  "figures_feb-2017_yam_tree.svg"\
   "figures_sep-2015_h1n1pdm_geo.png"\
   "figures_sep-2015_h1n1pdm_ha1-84_ha1-164_frequencies.png"\
   "figures_sep-2015_h1n1pdm_ha2-164_ha1-84.png"\
