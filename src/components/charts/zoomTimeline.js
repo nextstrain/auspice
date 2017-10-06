7fcd9d0632b25741ac546ea4c2ebf13ec73534aa
sidneymbell <sidneymb@uw.edu>
2017-10-06 14:37:10 -0700

    Semi-functional skeleton component for zoomable stacked bar chart

---
+++ src/components/charts/zoomTimeline.js
@@ -0,0 +1,160 @@
+import React from "react";
+import { connect } from "react-redux";
+// import * as globals from "../../util/globals";
+import Card from "../framework/card";
+import computeResponsive from "../../util/computeResponsive";
+import getColorScale from "../../util/getColorScale"
+import drawTimeline from "./zoomTimelineD3.js"
+import _ from "lodash";
+// import ceil from "Math";
+
+
+@connect(state => {
+  return {
+    nodes: state.tree.nodes,
+    visibility: state.tree.visibility,
+    nodeColors: state.tree.nodeColors,
+    browserDimensions: state.browserDimensions.browserDimensions,
+    colorBy: state.controls.colorBy,
+    colorScale: state.controls.colorScale,
+    absoluteDateMin: state.controls.absoluteDateMin,
+    absoluteDateMax: state.controls.absoluteDateMax,
+  };
+})
+
+class Timeline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: null,
+      visibility: false,
+      nodeColors: null,
+      browserDimensions: null,
+      colorBy: null,
+      chartGeom: this.getChartGeom(),
+    };
+  }
+
+  getChartGeom(props) {
+    const responsive = computeResponsive({
+      horizontal: 1,
+      vertical: 1,
+      browserDimensions: this.props.browserDimensions,
+      sidebar: this.props.sidebar
+    });
+    return {
+      responsive,
+      width: responsive.width,
+      height: responsive.height,
+      padBottom: 50,
+      padLeft: 15,
+      padRight: 12
+    };
+  }
+
+
+  // componentDidMount(props) {
+  // }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.nodes[0] && nextProps.visibility && nextProps.nodeColors) {
+    let tallies = this.getCounts(nextProps)
+    drawTimeline(tallies.counts, tallies.colors,this.state.chartGeom.responsive.width,this.state.chartGeom.responsive.height)
+  }
+    // this.maybeComputeResponive(nextProps);
+  }
+
+  // componentDidUpdate(prevProps, prevState) {
+  // }
+
+  getCounts(props) {
+
+    // pull all the visible tips in the tree, extract date and color value
+    const all_nodes = _.zip(props.visibility, props.nodes, props.nodeColors) // [ ["visible", {node}, '#000000'] ]
+    let visible_tips = []
+    let all_colors = []
+    _.each(all_nodes, (n)=>{
+      if (n[1].hasChildren===false && n[0]==="visible") {
+        visible_tips.push([n[1].attr['date'], n[2]]) // [ (1976-03-12, #000000) ]
+        all_colors.push(n[2])
+      }
+    });
+
+    let colors = _.uniq(all_colors)
+
+    // determine the number of months in each bin, convert from a date to a bin index
+    var parseDate = d3.time.format("%Y-%m-%d").parse;
+    let monthDiff = (d1, d2) => {
+        let year1 = d1.getYear()
+        let year2 = d2.getYear()
+        let month1 = d1.getMonth()
+        let month2 = d2.getMonth()
+        let numberOfMonths = (year2 - year1) * 12 + (month2 - month1) + 1;
+        return numberOfMonths;
+    }
+
+    let getNBins = (absoluteMonthDiff) => {
+      if (absoluteMonthDiff <= 120) {
+        return absoluteMonthDiff
+      } else if (absoluteMonthDiff < 1440) {
+        return _.ceil(absoluteMonthDiff / 12)
+      } else {
+        return 120
+      }}
+
+    let absoluteDateMin = parseDate(props.absoluteDateMin)
+    let absoluteDateMax = parseDate(props.absoluteDateMax)
+    let absoluteMonthDiff = monthDiff(absoluteDateMin, absoluteDateMax)
+    let nbins = getNBins(absoluteMonthDiff)
+    let months_per_bin = _.ceil(absoluteMonthDiff / nbins)
+
+    let date_to_month = (date) => {
+      // return month as integer index, where 0 == first month in datemin
+      return monthDiff(absoluteDateMin, date)
+    }
+
+    let month_to_bin = (date, months_per_bin) => {
+      let month_idx = date_to_month(date)
+      let bin_idx = _.ceil(month_idx / months_per_bin)
+      return bin_idx
+    }
+
+    let counts = []
+    let all_bins = _.range(nbins+1)
+    _.forEach(all_bins, (idx)=>{
+      counts[idx] = {}
+      _.forEach(colors, (color)=>{
+        counts[idx][color] = 0
+      })
+    })
+
+    _.forEach(visible_tips, (n)=>{
+      let bin_idx = month_to_bin(parseDate(n[0]), months_per_bin)
+      let category = n[1]
+        counts[bin_idx][category] += 1
+    })
+
+    const sumValues = obj => Object.values(obj).reduce((a, b) => a + b)
+    _.forEach(all_bins, (bin)=> {
+      if (!_.isEmpty(counts[bin])) {
+      counts[bin]['total'] = sumValues(counts[bin])
+    }
+      counts[bin]['x'] = bin
+    })
+    return {'counts': counts, 'colors': colors}
+  }
+
+  render() {
+    return (
+      <Card>
+      <div id="timelineAttachPointD3"
+      style={{width: this.state.chartGeom.responsive.width,
+              height: this.state.chartGeom.height}}
+      >
+      </div>
+        </Card>
+    );
+  }
+}
+
+export default Timeline;
---
+++ src/components/charts/zoomTimelineD3.js
@@ -0,0 +1,150 @@
+import d3 from "d3"
+
+function drawTimeline(data, colors, chartWidth, chartHeight) {
+
+data = _.map(data, parse)
+
+// sizing information, including margins so there is space for labels, etc
+var margin =  { top: 20, right: 20, bottom: 100, left: 20 },
+		width = chartWidth - margin.left - margin.right,
+		height = chartHeight - margin.top - margin.bottom,
+		marginOverview = { top: chartHeight-70, right: margin.right, bottom: 20,  left: margin.left },
+		heightOverview = chartHeight - marginOverview.top - marginOverview.bottom;
+
+// mathematical scales for the x and y axes
+var x = d3.time.scale()
+								.range([0, width]);
+var y = d3.scale.linear()
+								.range([height, 0]);
+var xOverview = d3.time.scale()
+								.range([0, width]);
+var yOverview = d3.scale.linear()
+								.range([heightOverview, 0]);
+
+// rendering for the x and y axes
+var xAxis = d3.svg.axis()
+								.scale(x)
+								.orient("bottom");
+var yAxis = d3.svg.axis()
+								.scale(y)
+								.orient("left");
+var xAxisOverview = d3.svg.axis()
+								.scale(xOverview)
+								.orient("bottom");
+
+// something for us to render the chart into
+var svg = d3.select("#timelineAttachPointD3")
+								.append("svg") // the overall space
+										.attr("width", width + margin.left + margin.right)
+										.attr("height", height + margin.top + margin.bottom);
+var main = svg.append("g")
+								.attr("class", "main")
+								.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var overview = svg.append("g")
+										.attr("class", "overview")
+										.attr("transform", "translate(" + marginOverview.left + "," + marginOverview.top + ")");
+
+// brush tool to let us zoom and pan using the overview chart
+var brush = d3.svg.brush()
+										.x(xOverview)
+										.on("brush", brushed);
+
+// setup complete, let's get some data!
+		// data ranges for the x and y axes
+		x.domain(d3.extent(data, function(d) { return d.x; }));
+		y.domain([0, d3.max(data, function(d) { return d.total; })]);
+		xOverview.domain(x.domain());
+		yOverview.domain(y.domain());
+
+		// data range for the bar colours
+		// (essentially maps attribute names to colour values)
+
+		// draw the axes now that they are fully set up
+		main.append("g")
+				.attr("class", "x axis")
+				.attr("transform", "translate(0," + height + ")")
+				.call(xAxis);
+		main.append("g")
+				.attr("class", "y axis")
+				.call(yAxis);
+		overview.append("g")
+				.attr("class", "x axis")
+				.attr("transform", "translate(0," + heightOverview + ")")
+				.call(xAxisOverview);
+
+		// draw the bars
+		main.append("g")
+						.attr("class", "bars")
+				// a group for each stack of bars, positioned in the correct x position
+				.selectAll(".bar.stack")
+				.data(data)
+				.enter().append("g") // for each bin
+						.attr("class", "bar stack")
+						.attr("transform", function(d) {
+							return "translate(" + d.x + ",0)";
+						})
+				// a bar for each value in the stack, positioned in the correct y positions
+				.selectAll("rect")
+				.data(function(d) {
+					return d.counts}) // for each category
+				.enter().append("rect")
+						.attr("class", "bar")
+						.attr("width", 6)
+						.attr("y", function(d) { return y(d.y1); })
+						.attr("height", function(d) { return y(d.y0) - y(d.y1); })
+						.style("fill", function(d) { return d.color; });
+
+		overview.append("g")
+								.attr("class", "bars")
+				.selectAll(".bar")
+				.data(data)
+				.enter().append("rect")
+						.attr("class", "bar")
+						.attr("x", function(d) { return xOverview(d.date) - 3; })
+						.attr("width", 6)
+						.attr("y", function(d) { return yOverview(d.total); })
+						.attr("height", function(d) { return heightOverview - yOverview(d.total); });
+
+		// add the brush target area on the overview chart
+		overview.append("g")
+								.attr("class", "x brush")
+								.call(brush)
+								.selectAll("rect")
+										// -6 is magic number to offset positions for styling/interaction to feel right
+										.attr("y", -6)
+										// need to manually set the height because the brush has
+										// no y scale, i.e. we should see the extent being marked
+										// over the full height of the overview chart
+										.attr("height", heightOverview + 7);  // +7 is magic number for styling
+
+// });
+
+// by habit, cleaning/parsing the data and return a new object to ensure/clarify data object structure
+function parse(d) {
+		var value = { 'x': d.x, 'total': d.total}; // turn the date string into a date object
+
+		// adding calculated data to each count in preparation for stacking
+		var y0 = 0; // keeps track of where the "previous" value "ended"
+		value.counts = colors.map(function(color) {
+				return { color: color,
+								 y0: y0,
+								 // add this count on to the previous "end" to create a range, and update the "previous end" for the next iteration
+								 y1: y0 += +d[color]
+							 };
+		});
+		return value;
+}
+
+// zooming/panning behaviour for overview chart
+function brushed() {
+		// update the main chart's x axis data range
+		x.domain(brush.empty() ? xOverview.domain() : brush.extent());
+		// redraw the bars on the main chart
+		main.selectAll(".bar.stack")
+						.attr("transform", function(d) { return "translate(" + x(d.date) + ",0)"; })
+		// redraw the x axis of the main chart
+		main.select(".x.axis").call(xAxis);
+}
+
+}
+export default drawTimeline
