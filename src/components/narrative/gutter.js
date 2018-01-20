76ebbe04e01a5d58a52bb83b9744f726c1c22d3c
james hadfield <jh22@sanger.ac.uk>
2018-01-19 22:11:47 -0800

    star-wars title text sizing

--- src/components/narrative/gutter.js
+++ src/components/narrative/gutter.js
@@ -13,9 +13,48 @@ const Chevron = ({pos, height, width, callback}) => {
   );
 };
 
+/* array of fontSizes and corresponding heights (px) */
+const fsH = [[23, 28], [19, 24], [16, 20], [15, 18], [13, 16], [11, 14]];
+
+/* fn to calculate the title sizes such that they are larger the closer they are to the
+in-focus title, and then diminish in size as they gt further off.
+If there is no remaining spaces, the titles are not displayed (fontsize & height = 0) */
+const calculateFontSizeAndHeightOfTitles = (titles, divHeight, visibility, gutterPos) => {
+  let spaceRemaining = 0.7 * divHeight;
+  const ret = [];
+
+  const makeFsH = (j) => {
+    let [fs, h] = j >= fsH.length ? fsH[fsH.length - 1] : fsH[j];
+    if (h > spaceRemaining) {
+      [fs, h] = [0, 0];
+    } else {
+      spaceRemaining -= h;
+    }
+    return [fs, h];
+  };
+
+  let j = 0; /* the 0-based numbering of the visibil elements */
+  if (gutterPos === "bottom") {
+    for (let i = 0; i < visibility.length; i++) {
+      const [fs, h] = visibility[i] === "hidden" ? [0, 0] : makeFsH(j);
+      if (visibility[i] === "visible") {j++;}
+      ret[i] = [fs, h + "px"];
+    }
+  } else {
+    for (let i = visibility.length - 1; i >= 0; i--) {
+      const [fs, h] = visibility[i] === "hidden" ? [0, 0] : makeFsH(j);
+      if (visibility[i] === "visible") {j++;}
+      ret[i] = [fs, h + "px"];
+    }
+  }
+  return [ret, spaceRemaining];
+};
+
 @connect()
 export class Gutter extends React.Component { // eslint-disable-line
   render() {
+    const useableHeight = this.props.height - 2 * 20;
+    const [vals, spaceRemaining] = calculateFontSizeAndHeightOfTitles(this.props.titles, useableHeight, this.props.visibility, this.props.pos);
     return (
       <div
         style={{
@@ -29,10 +68,12 @@ export class Gutter extends React.Component { // eslint-disable-line
         }}
       >
         <Chevron pos={this.props.pos} height={this.props.height} width={this.props.width} callback={this.props.callback}/>
-        {this.props.titles.map((t, i) => {
-          const style = this.props.visibility[i] === "hidden" ? {height: "0px", opacity: "0"} : {};
-          return (<div key={t} style={style}>{t}</div>);
-        })}
+        {this.props.pos === "bottom" ? <div style={{minHeight: (0.2 * useableHeight + spaceRemaining) + "px"}}/> : null}
+        {this.props.titles.map((t, i) => (
+          <div key={t} style={{transition: "all .3s ease", fontSize: vals[i][0], height: vals[i][1], margin: 0}}>
+            {this.props.visibility[i] === "hidden" ? '' : t}
+          </div>
+        ))}
       </div>
     );
   }
