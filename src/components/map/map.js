e69e2e44087bac413d1390903db92256e964113c
James Hadfield <jh22@sanger.ac.uk>
2018-01-24 18:45:19 -0800

    save animation params to URL state

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -494,11 +494,11 @@ class Map extends React.Component {
     ANIMATE MAP (AND THAT LINE ON TREE)
     **************************************** */
     if (this.props.mapAnimationPlayPauseButton === "Play") {
+      this.animateMap();
       this.props.dispatch({
         type: MAP_ANIMATION_PLAY_PAUSE_BUTTON,
         data: "Pause"
       });
-      this.animateMap();
     } else {
       if (enableAnimationPerfTesting) { window.Perf.resetCount(); }
       clearInterval(window.NEXTSTRAIN.mapAnimationLoop);
