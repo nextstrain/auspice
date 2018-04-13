93a0c3c780778bc01066097b4348446aa84f9eba
James Hadfield <jh22@sanger.ac.uk>
2018-04-12 19:12:39 -0700

    header & allow staging server

--- src/components/status/index.js
+++ src/components/status/index.js
@@ -6,8 +6,7 @@ import SingleDataset from "./single";
 
 @connect((state) => {
   return {
-    availableDatasets: state.datasets.availableDatasets,
-    s3bucket: state.datasets.s3bucket
+    availableDatasets: state.datasets.availableDatasets
   };
 })
 class Status extends React.Component {
@@ -29,17 +28,22 @@ class Status extends React.Component {
     exploreLevel([], this.props.availableDatasets.pathogen);
     return queries;
   }
-
   render() {
     if (!this.props.availableDatasets) {
       return null;
     }
+    const s3bucket = window.location.pathname.includes("staging") ? "staging" : "live";
     const queries = this.getAllQueries();
     return (
       <div>
         <TitleBar dataNameHidden/>
+
+        <h1 style={{margin: "20px 20px 20px 20px", textAlign: "center"}}>
+          {`Status page (s3 bucket: ${s3bucket})`}
+        </h1>
+
         {queries.map((q) => (
-          <SingleDataset key={q} path={q.join("_")} bucket={this.props.s3bucket}/>
+          <SingleDataset key={q} path={q.join("_")} bucket={s3bucket}/>
         ))}
       </div>
     );
