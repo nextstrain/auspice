84e669ea322879ced972ae38611d5332cf1c167b
Trevor Bedford <trevor@bedford.io>
2018-05-08 18:03:23 -0700

    Restore sidebar toggle on desktop

--- src/components/framework/sidebar-toggle.js
+++ src/components/framework/sidebar-toggle.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { goColor } from "../../globalStyles";
+import { controlsWidth } from "../../util/globals";
+
+/*
+sidebarOpen and !mobileDisplay: chevron inset in sidebar
+!sidebarOpen and !mobileDisplay: tab with chevron in top left of display
+sidebarOpen and mobileDisplay: green button with "close" icon
+!sidebarOpen and mobileDisplay: green button with "sliders" icon
+*/
 
 const SidebarToggle = ({sidebarOpen, mobileDisplay, handler}) => {
 
   const containerStyle = {
-    width: mobileDisplay ? 60 : 0,
-    height: mobileDisplay ? 60 : 0,
+    width: mobileDisplay ? 60 : 15,
+    height: mobileDisplay ? 60 : 15,
     position: "absolute",
-    right: 20,
-    top: 15,
+    top: mobileDisplay ? 15 : 0,
+    left: mobileDisplay ? "auto" : sidebarOpen ? controlsWidth + 24 : 0,
+    right: mobileDisplay ? 20 : "auto",
     zIndex: 9000,
-    backgroundColor: goColor,
-    boxShadow: "2px 4px 10px 1px rgba(0, 0, 0, 0.15)",
+    backgroundColor: mobileDisplay ? goColor : "#E1E1E1",
+    boxShadow: mobileDisplay ? "2px 4px 10px 1px rgba(0, 0, 0, 0.15)" : "none",
     cursor: "pointer",
     padding: 0,
-    transition: "top 0.4s ease-out, left 0.4s ease-out, width 0.4s ease-out, height 0.4s ease-out",
-    borderRadius: "45px"
+    transition: "top 0.3s ease-out, left 0.3s ease-out, width 0.3s ease-out, height 0.3s ease-out",
+    borderRadius: mobileDisplay ? "45px" : sidebarOpen ? "0px 0px 0px 2px" : "0px 0px 2px 0px"
   };
 
   const iconStyle = {
@@ -30,16 +39,24 @@ const SidebarToggle = ({sidebarOpen, mobileDisplay, handler}) => {
     marginLeft: "auto",
     marginRight: "auto",
     verticalAlign: "middle",
-    color: "#FFFFFF",
-    fontSize: 26
+    color: mobileDisplay ? "#FFFFFF" : "#444",
+    fontSize: mobileDisplay ? 26 : 12
   };
+
+  let iconClass = "fa fa-close";
+  if (sidebarOpen && !mobileDisplay) {
+    iconClass = "fa fa-chevron-left";
+  } else if (!sidebarOpen && !mobileDisplay) {
+    iconClass = "fa fa-chevron-right";
+  } else if (sidebarOpen && mobileDisplay) {
+    iconClass = "fa fa-close";
+  } else if (!sidebarOpen && mobileDisplay) {
+    iconClass = "fa fa-sliders";
+  }
 
   return (
     <div style={containerStyle} onClick={handler}>
-      {sidebarOpen ?
-        <div style={iconStyle}><i className={"fa fa-times"} aria-hidden="true"/></div> :
-        <div style={iconStyle}><i className={"fa fa-sliders"} aria-hidden="true"/></div>
-      }
+      <div style={iconStyle}><i className={iconClass} aria-hidden="true"/></div>
     </div>
   );
 };
