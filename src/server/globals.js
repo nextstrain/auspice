505e232fb451018a2ece5247a6da356cf58844d6
james hadfield <jh22@sanger.ac.uk>
2018-05-10 12:25:53 -0700

    fix server bug when serving local files

--- src/server/globals.js
+++ src/server/globals.js
@@ -2,11 +2,11 @@ const path = require("path");
 
 const setGlobals = ({localStatic=false, localData=false} = {}) => {
   global.LOCAL_DATA = localData;
-  global.LOCAL_DATA_PATH = path.join(__dirname, "/data/");
+  global.LOCAL_DATA_PATH = path.join(__dirname, "..", "..", "/data/");
   global.REMOTE_DATA_LIVE_BASEURL = "http://data.nextstrain.org/";
   global.REMOTE_DATA_STAGING_BASEURL = "http://staging.nextstrain.org/";
   global.LOCAL_STATIC = localStatic;
-  global.LOCAL_STATIC_PATH = path.join(__dirname, "/static/");
+  global.LOCAL_STATIC_PATH = path.join(__dirname, "..", "..", "/static/");
   global.REMOTE_STATIC_BASEURL = "http://cdn.rawgit.com/nextstrain/nextstrain.org/master/";
 };
 
