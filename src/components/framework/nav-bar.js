acc4ccdc8da9ab4e35db91d1c47240a768aff9d9
Trevor Bedford <trevor@bedford.io>
2018-05-14 21:20:44 -0700

    Add lower-level links to navbar to prevent splash page flash

--- src/components/framework/nav-bar.js
+++ src/components/framework/nav-bar.js
@@ -156,8 +156,8 @@ class NavBar extends React.Component {
         {this.getLogo(styles)}
         {this.getLogoType(styles)}
         <div style={{flex: 5}}/>
-        {this.getLink("About", "/about", styles)}
-        {this.getLink("Docs", "/docs", styles)}
+        {this.getLink("About", "/about/overview/introduction", styles)}
+        {this.getLink("Docs", "/docs/getting-started/install", styles)}
         {this.getLink("Blog", "/blog", styles)}
         {this.getChevron()}
         <div style={{width: this.props.minified ? 8 : 0 }}/>
