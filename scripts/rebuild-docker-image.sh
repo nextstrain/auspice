81b50db043ad0f09e945a85f98a0367c56eafa0f
Trevor Bedford <trevor@bedford.io>
2018-07-15 15:43:01 -0700

    Ping Travis CI rather than Docker Hub for image builds

--- scripts/rebuild-docker-image.sh
+++ scripts/rebuild-docker-image.sh
@@ -1,5 +1,15 @@
-echo "Pinging Docker Hub to rebuild image"
+echo "Pinging Travis CI to rebuild Docker image"
 
-curl -n -X POST https://registry.hub.docker.com/u/nextstrain/base/trigger/$DOCKER_AUTH_TOKEN/ \
---data '{"build": true}' \
--H "Content-Type: application/json"
+body='{
+  "request": {
+    "branch": "master",
+    "message": "Build triggered from auspice"
+  }
+}'
+
+curl -X POST https://api.travis-ci.com/repo/nextstrain%2Fdocker-base/requests \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token $TRAVIS_AUTH_TOKEN" \
+  -d "$body"
