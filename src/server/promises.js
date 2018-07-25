6e7daf318e6eb318ef09a6ceeaed44d9acf50e90
James Hadfield <jh22@sanger.ac.uk>
2018-07-25 13:23:03 -0700

    Server logs content-encoding of fetched JSONs

--- src/server/promises.js
+++ src/server/promises.js
@@ -1,3 +1,5 @@
+/* eslint no-console: off */
+
 const fetch = require('node-fetch');
 const fs = require('fs');
 
@@ -5,6 +7,9 @@ const fetchJSON = (path) => {
   const p = fetch(path)
     .then((res) => {
       if (res.status !== 200) throw new Error(res.statusText);
+      const ctype = res.headers[Object.getOwnPropertySymbols(res.headers)[0]]["content-type"];
+      const cenc = res.headers[Object.getOwnPropertySymbols(res.headers)[0]]["content-encoding"] || "none";
+      console.log(`\tGot type ${ctype} with encoding ${cenc}`);
       return res;
     })
     .then((res) => res.json());
