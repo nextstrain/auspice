5be41a64856db09d99eba637ffb3d2ea88c24446
James Hadfield <jh22@sanger.ac.uk>
2018-07-24 21:00:42 -0700

    Allow local data directory command line override
    npm run server -- local:PATH or node server.js local:PATH
    where PATH is relative to the auspice directory

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ const charon = require("./src/server/charon");
 const globals = require("./src/server/globals");
 
 /* documentation in the static site! */
-
 const devServer = process.argv.indexOf("dev") !== -1;
-globals.setGlobals({
-  localData: process.argv.indexOf("localData") !== -1
-});
+globals.setGlobals(process.argv);
 
 /* if we are in dev-mode, we need to import specific libraries & set flags */
 let webpack, config, webpackDevMiddleware, webpackHotMiddleware;
@@ -53,6 +50,6 @@ const server = app.listen(app.get('port'), () => {
   console.log("-----------------------------------");
   console.log("Auspice server started on port " + server.address().port);
   console.log(devServer ? "Serving dev bundle with hot-reloading enabled" : "Serving compiled bundle from /dist");
-  console.log(global.LOCAL_DATA ? `Data is being sourced from ${global.LOCAL_DATA_PATH}` : "Dataset JSONs are being sourced from S3, narratives via the static github repo");
+  console.log(`Local data is sourced from ${global.LOCAL_DATA_PATH} can be accessed via "/local/..." URLs`);
   console.log("-----------------------------------\n\n");
 });
--- src/server/globals.js
+++ src/server/globals.js
@@ -1,10 +1,23 @@
 /* eslint no-console: off */
 const path = require("path");
+const fs = require('fs');
 const manifestHelpers = require("./manifestHelpers");
 
-const setGlobals = ({localData=false} = {}) => {
-  global.LOCAL_DATA = localData;
-  global.LOCAL_DATA_PATH = path.resolve(__dirname, "..", "..", "data/");
+const setGlobals = (argv) => {
+  /* do we have a custom local data directory? */
+  global.LOCAL_DATA_PATH = path.resolve(__dirname, "..", "..", "data/"); /* default */
+  const customLocalDir = argv.filter((x) => x.startsWith("local"));
+  if (customLocalDir.length === 1 && customLocalDir[0].indexOf(":") !== -1) {
+    let ldp = customLocalDir[0].split(":")[1];
+    if (!ldp.endsWith("/")) ldp += "/";
+    ldp = path.resolve(ldp);
+    if (!fs.existsSync(ldp)) {
+      console.log(`ERROR -- local directory ${ldp} wasn't found. Falling back to default.`);
+    } else {
+      global.LOCAL_DATA_PATH = ldp;
+    }
+  }
+
   global.REMOTE_DATA_LIVE_BASEURL = "http://data.nextstrain.org";
   global.REMOTE_DATA_STAGING_BASEURL = "http://staging.nextstrain.org";
   global.REMOTE_STATIC_BASEURL = "http://cdn.rawgit.com/nextstrain/static/master";
