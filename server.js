66b8c304b583c0c48ee1e4dd19b7aada7e1a0ed6
Trevor Bedford <trevor@bedford.io>
2018-10-20 17:49:14 +0200

    Experiment with Passport JS

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const globals = require("./src/server/globals");
 const compression = require('compression');
 const argparse = require('argparse');
 const version = require('./src/version').version;
+const passport = require("passport");
+const GoogleStrategy = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
 
 const parser = new argparse.ArgumentParser({
   version: version,
@@ -41,9 +44,51 @@ if (args.dev) {
   webpackHotMiddleware = require("webpack-hot-middleware"); // eslint-disable-line
 }
 
+
 const app = express();
 app.set('port', process.env.PORT || 4000);
 app.use(compression());
+
+// cookieSession config
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000, // One day in milliseconds
+  keys: ['randomstringhere']
+}));
+
+app.use(passport.initialize()); // Used to initialize passport
+app.use(passport.session()); // Used to persist login sessions
+
+// Strategy config
+passport.use(new GoogleStrategy(
+  {
+    clientID: '',
+    clientSecret: '',
+    callbackURL: 'https://lvh.me'
+  },
+  (accessToken, refreshToken, profile, done) => {
+    done(null, profile); // passes the profile data to serializeUser
+  }
+));
+
+// Used to stuff a piece of information into a cookie
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+// Used to decode the received cookie and persist session
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+// Middleware to check if the user is authenticated
+function isUserAuthenticated(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.send('You must login!');
+    }
+}
+
 
 if (args.dev) {
   const compiler = webpack(config);
@@ -64,11 +109,26 @@ app.get("/favicon.png", (req, res) => {
   res.sendFile(path.resolve(__dirname, "favicon.png"));
 });
 
-charon.applyCharonToApp(app);
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+// Routes
+app.get('/', (req, res) => {
+    res.render('index.ejs');
 });
+
+// passport.authenticate middleware is used here to authenticate the request
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile'] // Used to specify the required data
+}));
+
+// Callback route
+app.get('/callback', isUserAuthenticated, (req, res) => {
+    res.send('You have reached the callback route');
+});
+
+// charon.applyCharonToApp(app);
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "index.html"));
+// });
 
 const server = app.listen(app.get('port'), () => {
   console.log("-----------------------------------");
