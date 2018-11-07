294073bceed4373b23087abaa0abbb664d632128
Thomas Sibley <tsibley@fredhutch.org>
2018-11-07 12:54:10 -0800

    Enable source maps for debugging production builds
    Though the development webpack config includes source maps, they were
    disabled in production. When debugging a live build (or a local build
    where you don't want hot reloading), it's very useful to have a source
    map for the browser to translate between the packed dist/bundle.js and
    the original code.
    
    This matches the recommendations of webpack's documentation:
    
        https://webpack.js.org/guides/production/#source-mapping

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,7 @@ const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   mode: 'production',
+  devtool: 'source-map',
   entry: [
     "babel-polyfill",
     "./src/index"
