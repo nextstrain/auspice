f6a576db40183193bf690b1f44261a999c37877d
james hadfield <jh22@sanger.ac.uk>
2019-01-02 15:09:03 +1300

    Update release instructions, docs
    Releasing auspice to npm no longer uploads the src to a s3 bucket nor triggers nextstrain.org to rebuild (this must be done from within the nextstrain.org repo). 
    When not extended, auspice is focused on local builds therefore is not to be run on Heroku.

--- scripts/gzip-and-upload.sh
+++
@@ -1,7 +0,0 @@
-echo "GZipping build to s3/"
-rm -r s3
-mkdir s3
-tar -czvf s3/auspice.tar.gz dist src/server index.html
-
-echo "Uploading to s3 nextstrain-bundles bucket"
-aws s3 cp s3/ s3://nextstrain-bundles/ --recursive --region=us-east-1
