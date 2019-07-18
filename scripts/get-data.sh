84fcdd8450170a517a209defe375b925a2d7fa3d
james hadfield <jh22@sanger.ac.uk>
2019-07-19 09:31:18 +1200

    remove images from get-data script
    These haven't been used since we moved the splash page to the static-site repo (which is now part of the nextstrain.org repo)

--- scripts/get-data.sh
+++ scripts/get-data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 data_files=(
   "manifest_guest.json" \
-  "img_mumps.png" "img_dengue.png" "img_avianinfluenza.png" "img_seasonalinfluenza.png" "img_ebola.png" "img_zika.png" "img_measles.png" "img_lassa.jpg" "img_wnv.png"\
   "dengue_all_tree.json" "dengue_all_meta.json" \
   "dengue_denv1_tree.json" "dengue_denv1_meta.json" \
   "dengue_denv2_tree.json" "dengue_denv2_meta.json" \
