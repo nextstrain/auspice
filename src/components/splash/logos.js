e0ecb9d6c884037975ec6ddc37e5845bbd586ce3
P-O Quirion <pioliqui@gmail.com>
2020-04-27 10:03:31 -0400

    force two lines on splash

--- src/components/splash/logos.js
+++ src/components/splash/logos.js
@@ -2,17 +2,17 @@
 import React from "react";
 
 export const logos = [
+  <a key={3} href="https://www.inspq.qc.ca/" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="180" src={require("../../images/inspq.jpg")}/>
+  </a>,
+  <a key={4} href="http://www.mcgillgenomecentre.org/" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="170" src={require("../../images/Genome_logo.png")}/>
+  </a>,
   <a key={1} href="https://c3g.ca/" target="_blank" rel="noreferrer noopener">
     <img alt="logo" width="165" src={require("../../images/c3g_source.png")}/>
   </a>,
   <a key={2} href="https://www.inspq.qc.ca/lspq" target="_blank" rel="noreferrer noopener">
     <img alt="logo" width="165" src={require("../../images/lspq.jpeg")}/>
-  </a>,
-  <a key={3} href="https://www.inspq.qc.ca/" target="_blank" rel="noreferrer noopener">
-    <img alt="logo" width="165" src={require("../../images/inspq.jpg")}/>
-  </a>,
-  <a key={4} href="http://www.mcgillgenomecentre.org/" target="_blank" rel="noreferrer noopener">
-    <img alt="logo" width="165" src={require("../../images/Genome_logo.png")}/>
   </a>,
   <a key={5} href="https://www.calculquebec.ca/" target="_blank" rel="noreferrer noopener">
     <img alt="logo" width="165" src={require("../../images/CalculQuebec_logo_medium.png")}/>
--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -105,9 +105,37 @@ const SplashContent = ({available, browserDimensions, dispatch, errorMessage, ch
 
   const Footer = () => (
     <CenterContent>
-      {logos}
+<span>
+  <a key={3} href="https://www.inspq.qc.ca/" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="200" src={require("../../images/inspq.jpg")}/>
+  </a>
+  <a key={4} href="http://www.mcgillgenomecentre.org/" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="200" src={require("../../images/Genome_logo.png")}/>
+  </a>
+  <a key={1} href="https://c3g.ca/" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="200" src={require("../../images/c3g_source.png")}/>
+  </a>
+</span>
+<span>
+  <a key={2} href="https://www.inspq.qc.ca/lspq" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="165" src={require("../../images/lspq.jpeg")}/>
+  </a>
+  <a key={5} href="https://www.calculquebec.ca/" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="165" src={require("../../images/CalculQuebec_logo_medium.png")}/>
+  </a>
+  <a key={6} href="https://www.mcgill.ca/" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="165" src={require("../../images/Mcgill_Logo.png")}/>
+  </a>
+  <a key={7} href="https://www.umontreal.ca/" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="165" src={require("../../images/UdeM-officiel-RVB.png")}/>
+  </a>
+</span>
     </CenterContent>
+
+
   );
+
+
 
   return (
     <>
