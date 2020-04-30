1c2493551aa9426e4a00dacdb0c19087c6e14535
Rom Grk <romgrk.cc@gmail.com>
2020-04-30 18:08:40 -0400

    store: add redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,27 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 import { changeURLMiddleware } from "../middleware/changeURL";
 import rootReducer from "../reducers";
 import { loggingMiddleware } from "../middleware/logActions"; // eslint-disable-line no-unused-vars
 
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
 const configureStore = (initialState) => {
   const middleware = [
     thunk,
-    changeURLMiddleware, // eslint-disable-line comma-dangle
-    // loggingMiddleware
+    changeURLMiddleware
   ];
+
+  if (isDevelopment)
+    middleware.push(logger)
+
   const composedEnhancers = compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
   );
   const store = createStore(rootReducer, initialState, composedEnhancers);
-  if (process.env.NODE_ENV !== 'production' && module.hot) {
+  if (isDevelopment && module.hot) {
     // console.log("hot reducer reload"); // eslint-disable-line
     module.hot.accept('../reducers', () => {
       const nextRootReducer = require('../reducers/index');  // eslint-disable-line global-require
