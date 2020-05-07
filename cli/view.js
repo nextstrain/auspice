8cdbaae58ff3e376300b07d23cc623618963dc6a
Allister <allisterkmoon@gmail.com>
2020-05-07 09:55:49 -0700

    catch EADDRINUSE and ENOTFOUND on dev/view script Catches both these events when starting the server on develop and view scripts, displays error to user.

--- cli/view.js
+++ cli/view.js
@@ -136,14 +136,21 @@ const run = (args) => {
     utils.log(auspiceBuild.message);
     utils.log(handlerMsg);
     utils.log("---------------------------------------------------\n\n");
-  }).on('error', (err) => {
-    if (err.code === 'EADDRINUSE') {
-      utils.error(`Port ${app.get('port')} is currently in use by another program. 
-      You must either close that program or specify a different port by setting the shell variable
-      "$PORT". Note that on MacOS / Linux, "lsof -n -i :${app.get('port')} | grep LISTEN" should
-      identify the process currently using the port.`);
+  }).on('error', (error) => {
+    if (error.code === 'EADDRINUSE') {
+      utils.error(`Port ${app.get('port')} is currently in use by another program.
+      You must either close that program or specify a different port by setting the shell variable "$PORT". Note that on MacOS / Linux ${chalk.yellow(`lsof -n -i :${app.get('port')} | grep LISTEN`)} should identify the process currently using the port.`);
     }
-    utils.error(`Uncaught error in app.listen(). Code: ${err.code}`);
+
+    if (error.code === 'ENOTFOUND') {
+      utils.error(`Host ${app.get('host')} is not a valid address. The server could not be started. If you did not provide a HOST environment variable when starting the app you may have HOST already set in your system. You can either change that variable, or override HOST when starting the app.
+
+      Example commands to fix:
+        ${chalk.yellow('HOST="localhost" auspice view')}
+        ${chalk.yellow('HOST="localhost" npm run view')}`);
+    }
+
+    utils.error(`Uncaught error in app.listen(). Code: ${error.code}`);
   });
 
 };
