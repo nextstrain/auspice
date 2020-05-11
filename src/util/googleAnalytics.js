9987764a06956675224199ca510bd67fc93e991b
salvatore-fxpig <salvatore@fxpig.com>
2020-05-11 15:30:43 +0200

    package size optimization - google analytics

--- src/util/googleAnalytics.js
+++ src/util/googleAnalytics.js
@@ -1,38 +1,43 @@
-import ReactGA from "react-ga"; /* https://github.com/react-ga/react-ga */
+// import ReactGA from "react-ga"; /* https://github.com/react-ga/react-ga */
 import { hasExtension, getExtension } from "./extensions";
 
+let ReactGA;
+let importReactGa;
 
-export const initialiseGoogleAnalyticsIfRequired = () => {
-  ReactGA.doNotUse = true; // a flag to indicate whether we are using GA
+export const initialiseGoogleAnalyticsIfRequired = async () => {
   if (!hasExtension("googleAnalyticsKey")) {
     return;
   }
+  importReactGa = import("react-ga");
+  ReactGA = (await importReactGa).default;
   if (process.env.NODE_ENV !== "production") {
     // eslint-disable-next-line
     console.log("Not setting up Google Analytics as we are not in production mode");
     return;
   }
-  ReactGA.doNotUse = false;
   ReactGA.initialize(getExtension("googleAnalyticsKey"));
 };
 
-export const analyticsNewPage = () => {
-  if (ReactGA.doNotUse) return;
+export const analyticsNewPage = async () => {
+  if (importReactGa) await importReactGa;
+  else return;
   // console.log("GA page change to", window.location.pathname)
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
 };
 
-export const analyticsControlsEvent = (action) => {
-  if (ReactGA.doNotUse) return;
+export const analyticsControlsEvent = async (action) => {
+  if (importReactGa) await importReactGa;
+  else return;
   ReactGA.event({
     category: "Controls",
     action
   });
 };
 
-export const triggerOutboundEvent = (address) => {
-  if (ReactGA.doNotUse) return;
+export const triggerOutboundEvent = async (address) => {
+  if (importReactGa) await importReactGa;
+  else return;
   ReactGA.outboundLink(
     {label: address},
     () => console.log("outbound event triggered", address) // eslint-disable-line no-console
