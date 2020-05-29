c238807ccfe423e7e47831ef5d6fa598b245266f
james hadfield <jh22@sanger.ac.uk>
2020-05-29 15:57:54 +1200

    Remove unnecessary byline in sidebar

--- src/components/controls/transmission-lines.js
+++ src/components/controls/transmission-lines.js
@@ -5,7 +5,6 @@ import { withTranslation } from "react-i18next";
 import Toggle from "./toggle";
 import { controlsWidth } from "../../util/globals";
 import { TOGGLE_TRANSMISSION_LINES } from "../../actions/types";
-import { SidebarSubtitle } from "./styles";
 
 @connect((state) => {
   return {
@@ -16,21 +15,16 @@ class TransmissionLines extends React.Component {
   render() {
     const { t } = this.props;
     return (
-      <>
-        <SidebarSubtitle spaceAbove>
-          {t("sidebar:Transmission lines")}
-        </SidebarSubtitle>
-        <div style={{marginBottom: 10, width: controlsWidth, fontSize: 14}}>
-          <Toggle
-            display
-            on={this.props.showTransmissionLines}
-            callback={() => {
-              this.props.dispatch({ type: TOGGLE_TRANSMISSION_LINES, data: !this.props.showTransmissionLines });
-            }}
-            label={t("sidebar:Show transmission lines")}
-          />
-        </div>
-      </>
+      <div style={{marginBottom: 10, width: controlsWidth, fontSize: 14}}>
+        <Toggle
+          display
+          on={this.props.showTransmissionLines}
+          callback={() => {
+            this.props.dispatch({ type: TOGGLE_TRANSMISSION_LINES, data: !this.props.showTransmissionLines });
+          }}
+          label={t("sidebar:Show transmission lines")}
+        />
+      </div>
     );
   }
 }
