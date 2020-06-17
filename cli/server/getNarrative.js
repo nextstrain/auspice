7a553b6353952f235afea99494248673fc031046
james hadfield <jh22@sanger.ac.uk>
2020-06-17 13:27:58 +1200

    [client-narratives] Allow narrative API to return unmodified markdown file
    This is in preparation for client-side parsing of narrative files. A optional `type` URL query allows the same API to return the markdown file (see the documentation changes in this commit for more details).
    
    There are other possibilities, such as using the content-type field in the header, but I felt this was the clearest.

--- cli/server/getNarrative.js
+++ cli/server/getNarrative.js
@@ -6,19 +6,32 @@ const utils = require("../utils");
 
 const setUpGetNarrativeHandler = ({narrativesPath}) => {
   return async (req, res) => {
-    const prefix = queryString.parse(req.url.split('?')[1]).prefix || "";
+    const query = queryString.parse(req.url.split('?')[1]);
+    const prefix = query.prefix || "";
     const filename = prefix
       .replace(/.+narratives\//, "")  // remove the URL up to (& including) "narratives/"
       .replace(/\/$/, "")             // remove ending slash
       .replace(/\//g, "_")            // change slashes to underscores
       .concat(".md");                 // add .md suffix
 
+    // the type-query string dictates whether to parse into JSON on the server or not
+    // we default to JSON which was behavior before this argument existed
+    const type = query.type ? query.type.toLowerCase() : "json";
+
     const pathName = path.join(narrativesPath, filename);
     utils.log("trying to access & parse local narrative file: " + pathName);
     try {
       const fileContents = fs.readFileSync(pathName, 'utf8');
-      const blocks = parseNarrative(fileContents);
-      res.send(JSON.stringify(blocks).replace(/</g, '\\u003c'));
+      if (type === "md" || type === "markdown") {
+        // we could stream the response (as we sometimes do for getDataset) but narrative files are small
+        // so the expected time savings / server overhead is small.
+        res.send(fileContents);
+      } else if (type === "json") {
+        const blocks = parseNarrative(fileContents);
+        res.send(JSON.stringify(blocks).replace(/</g, '\\u003c'));
+      } else {
+        throw new Error(`Unknown format requested: ${type}`);
+      }
       utils.verbose("SUCCESS");
     } catch (err) {
       const errorMessage = `Narratives couldn't be served -- ${err.message}`;
