3a6202e0fc3daf81cd25652d16827b8023d0b1b5
Thomas Sibley <tsibley@fredhutch.org>
2020-07-09 10:44:24 -0700

    Remove unused fetchJSON() function from CLI utils
    All existing callers of fetchJSON() use the version in
    src/utils/serverInteraction.js.  I also checked the nextstrain.org (uses
    its own fetchJSON()) and auspice.us (doesn't call fetchJSON())
    codebases.

--- cli/utils.js
+++ cli/utils.js
@@ -59,22 +59,6 @@ const resolveLocalDirectory = (providedPath, isNarratives) => {
   return localPath;
 };
 
-const fetchJSON = (pathIn) => {
-  const p = fetch(pathIn)
-    .then((res) => {
-      if (res.status !== 200) throw new Error(res.statusText);
-      try {
-        const header = res.headers[Object.getOwnPropertySymbols(res.headers)[0]] || res.headers._headers;
-        verbose(`Got type ${header["content-type"]} with encoding ${header["content-encoding"] || "none"}`);
-      } catch (e) {
-        // potential errors here are inconsequential for the response
-      }
-      return res;
-    })
-    .then((res) => res.json());
-  return p;
-};
-
 const readFilePromise = (fileName) => {
   return new Promise((resolve, reject) => {
     fs.readFile(fileName, 'utf8', (err, data) => {
@@ -126,7 +110,6 @@ module.exports = {
   error,
   resolveLocalDirectory,
   customOutputPath,
-  fetchJSON,
   readFilePromise,
   exportIndexDotHtml
 };
