0e5ec804e93deefd350a6c0490366abeeac043ac
eharkins <eli.harkins@gmail.com>
2020-11-04 20:16:09 -0800

    exclude unused docs from build

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,10 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md',
+    'narratives',
+    'contributing',
+]
 
 
 # -- Options for HTML output -------------------------------------------------
