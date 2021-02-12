cb42f97b1b7100c5f5178e557f98ea17151c800b
babarlelephant <acx01b@yahoo.fr>
2021-02-12 12:43:57 +0100

    Create hover.js
    ....

--- src/components/tree/infoPanels/hover.js
+++ src/components/tree/infoPanels/hover.js
@@ -150,7 +150,7 @@ const Mutations = ({node, t}) => {
   /* --------- NUCLEOTIDE MUTATIONS --------------- */
   /* Nt mutations are found at `mutations.nuc` -> Array of strings */
   if (mutations.nuc && mutations.nuc.length) {
-    const nDisplay = 21; // max number of mutations to display
+    const nDisplay = 25; // max number of mutations to display
 
     const isMutGap = (mut) => mut.slice(-1) === "-" || mut.slice(0, 1) === "-";
     const isMutUnknown = (mut) => mut.slice(-1) === "N" || mut.slice(0, 1) === "N";
@@ -196,7 +196,7 @@ const Mutations = ({node, t}) => {
     }
   }
   if (shouldDisplay) {
-    const nDisplay = 10; // number of mutations to display per protein
+    const nDisplay = 12; // number of mutations to display per protein
     const nProtsToDisplay = 8; // max number of proteins to display
     const mutationsToRender = [];
     Object.keys(mutationsToDisplay).forEach((prot, idx) => {
