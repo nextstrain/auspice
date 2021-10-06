3f205d0db3814c115ae867ab7a98144f4baa00c6
Victor Lin <13424970+victorlin@users.noreply.github.com>
2021-10-05 21:46:16 -0700

    fix whitespace formatting

--- src/components/controls/date-picker.js
+++ src/components/controls/date-picker.js
@@ -13,21 +13,21 @@ const DateInput = styled.input`
   float: ${(props) => props.right ? "right" : "left"};
 `;
 
-const DatePicker = ({value, onChange, right}) => {
-    const [editing, setEditing] = React.useState(false);
-    function processNewValue(newValue) {
-        setEditing(false);
-        onChange(newValue);
+const DatePicker = ({ value, onChange, right }) => {
+  const [editing, setEditing] = React.useState(false);
+  function processNewValue(newValue) {
+    setEditing(false);
+    onChange(newValue);
+  }
+  function onKeyUp(e) {
+    if (e.key === 'Enter') {
+      processNewValue(e.target.value);
     }
-    function onKeyUp(e) {
-        if (e.key === 'Enter') {
-            processNewValue(e.target.value);
-        }
-    }
-    if (editing) {
-        return <DateInput type="text" defaultValue={value} onKeyUp={onKeyUp} right={right} />;
-    }
-    return <DateLabel onClick={() => setEditing(true)} right={right}>{value}</DateLabel>;
+  }
+  if (editing) {
+    return <DateInput type="text" defaultValue={value} onKeyUp={onKeyUp} right={right} />;
+  }
+  return <DateLabel onClick={() => setEditing(true)} right={right}>{value}</DateLabel>;
 };
 
 export default DatePicker;
