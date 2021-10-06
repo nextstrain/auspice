ff96f76fd3ab25a7436f1f0ab51d069d0321c8a4
Victor Lin <13424970+victorlin@users.noreply.github.com>
2021-10-06 12:11:46 -0700

    rearrange/rename styled components

--- src/components/controls/date-picker.js
+++ src/components/controls/date-picker.js
@@ -3,23 +3,23 @@ import styled from 'styled-components';
 import { MdDateRange } from "react-icons/md";
 import { FiSave } from "react-icons/fi";
 
+const FloatDiv = styled.div`
+  float: ${(props) => props.right ? "right" : "left"};
+  max-width: 50%;
+`;
+const IconSpan = styled.span`
+  font-family: ${(props) => props.theme["font-family"]};
+  font-size: 12px;
+  font-weight: 400;
+  color: ${(props) => props.theme.color};
+  padding: 0 5px;
+`;
 const DateLabel = styled.div`
   font-family: ${(props) => props.theme["font-family"]};
   margin-bottom: 5px;
   font-size: 12px;
   font-weight: 400;
   color: ${(props) => props.theme.color};
-`;
-const FloatDiv = styled.div`
-  float: ${(props) => props.right ? "right" : "left"};
-  max-width: 50%;
-`;
-const CalendarSpan = styled.span`
-  font-family: ${(props) => props.theme["font-family"]};
-  font-size: 12px;
-  font-weight: 400;
-  color: ${(props) => props.theme.color};
-  padding: 0 5px;
 `;
 
 const DatePicker = ({ value, minDate, maxDate, onChange, right }) => {
@@ -45,11 +45,11 @@ const DatePicker = ({ value, minDate, maxDate, onChange, right }) => {
       onKeyUp={onKeyUp}
       onBlur={onBlur}
     />;
-    const saveIcon = <CalendarSpan onClick={() => setEditing(false)}><FiSave /></CalendarSpan>;
+    const saveIcon = <IconSpan onClick={() => setEditing(false)}><FiSave /></IconSpan>;
     const labelValue = right ? <>{inputElement}{saveIcon}</> : <>{saveIcon}{inputElement}</>;
     return <FloatDiv right={right}>{labelValue}</FloatDiv>;
   }
-  const calenderIcon = <CalendarSpan onClick={() => setEditing(true)}><MdDateRange /></CalendarSpan>;
+  const calenderIcon = <IconSpan onClick={() => setEditing(true)}><MdDateRange /></IconSpan>;
   const labelValue = right ? <>{value}{calenderIcon}</> : <>{calenderIcon}{value}</>;
   return <FloatDiv right={right}>
     <DateLabel>{labelValue}</DateLabel>
