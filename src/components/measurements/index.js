f03a764eeea475ded59b211f7b27d61b433644de
Jover <joverlee521@gmail.com>
2022-01-28 20:57:05 -0800

    Add D3 SVG to Measurements panel
    Only displays measurements that are linked to samples currently visible
    on the tree, so each measurement's "strain" value must exactly match
    the strain name of the sample in the tree. This ensures that filters of
    the tree also filters data of the multiplot.
    
    Since re-drawing the SVG can be pretty slow, I have taken a couple steps
    to memoize the data for the SVG and avoid re-rendering/re-drawing
    when not needed:
    
    1. Used `lodash.isEqual` for the React Redux selectors equality checks
    for Redux states that are objects. The default is a strict `===`
    reference equality check that would fail for objects and cause the
    panel to re-render every time an action is dispatched.
    
    2. Used `useMemo` to memoize the D3 scale functions. The D3 scales only
    need to be updated when the collection or width has changed, so use
    memo to preserve it across renders. Memomizing the functions also allow
    them to be used as dependencies of `useEffect` hooks without causing
    the hooks to re-run every render.
    
    3. Used a custom `useDeepCompareMemo()` function to memoize all the data
    needed for drawing the SVG. This is done in anticipation that following
    commits will introduce other `useEffect` hooks that will also need
    `svgData` as a dependency (e.g. colors will need to be re-applied when
    `svgData` changes).

--- src/components/measurements/index.js
+++ src/components/measurements/index.js
@@ -1,9 +1,95 @@
-import React from "react";
+import React, { useRef, useEffect, useMemo } from "react";
+import { useSelector } from "react-redux";
+import { isEqual } from "lodash";
+import { NODE_VISIBLE } from "../../util/globals";
 import ErrorBoundary from "../../util/errorBoundry";
 import Card from "../framework/card";
 import Legend from "../tree/legend/legend";
+import {
+  createXScale,
+  createYScale,
+  groupMeasurements,
+  clearMeasurementsSVG,
+  drawMeasurementsSVG
+} from "./measurementsD3";
+
+/**
+ * A custom React Hook that returns a memoized value that will only change
+ * if a deep comparison using lodash.isEqual determines the value is not
+ * equivalent to the previous value.
+ * @param {*} value
+ * @returns {*}
+ */
+const useDeepCompareMemo = (value) => {
+  const ref = useRef();
+  if (!isEqual(value, ref.current)) {
+    ref.current = value;
+  }
+  return ref.current;
+};
+
+/**
+ * A custom React Redux Selector that reduces the tree redux state to an object
+ * with the terminal strain names and their visibility.
+ *
+ * Depends on tree.visibility having the visibility values in an array that
+ * has the same order as tree.nodes
+ * @param {Object} tree
+ * @returns {Object<string,number>}
+ */
+const treeStrainVisibilitySelector = (tree) => {
+  return tree.nodes.reduce((treeStrainVisibility, node, index) => {
+    // Only store visibility of terminal strain nodes
+    if (!node.hasChildren) {
+      treeStrainVisibility[node.name] = tree.visibility[index];
+    }
+    return treeStrainVisibility;
+  }, {});
+};
+
+/**
+ * Filters provided measurements to only measurements for strains that are
+ * currently visible in the tree. Visibiity is indicated by the NODE_VISIBLE
+ * value in the provided treeStrainVisibility object for strains.
+ * @param {Array<Object>} measurements
+ * @param {Object<string,number>} treeStrainVisibility
+ * @returns {Array<Object>}
+ */
+const filterToTreeVisibileStrains = (measurements, treeStrainVisibility) => {
+  // Check visibility against global NODE_VISIBLE
+  const isVisible = (visibility) => visibility === NODE_VISIBLE;
+  return measurements.filter((m) => isVisible(treeStrainVisibility[m.strain]));
+};
 
 const Measurements = ({height, width, showLegend}) => {
+  // Use `lodash.isEqual` to deep compare object states to prevent unnecessary re-renderings of the component
+  const treeStrainVisibility = useSelector((state) => treeStrainVisibilitySelector(state.tree), isEqual);
+  const groupBy = useSelector((state) => state.controls.measurementsGroupBy);
+  const collection = useSelector((state) => state.measurements.collectionToDisplay, isEqual);
+  const { title, x_axis_label, threshold, fields, measurements } = collection;
+
+  // Ref to access the D3 SVG
+  const d3Ref = useRef(null);
+
+  // Filter and group measurements
+  const filteredMeasurements = filterToTreeVisibileStrains(measurements, treeStrainVisibility);
+  const groupedMeasurements = groupMeasurements(filteredMeasurements, groupBy);
+
+  // Memoize D3 scale functions to allow deep comparison to work below for svgData
+  const xScale = useMemo(() => createXScale(width, measurements), [width, measurements]);
+  const yScale = useMemo(() => createYScale(measurements), [measurements]);
+  // Memoize all data needed for basic SVG to avoid extra re-drawings
+  const svgData = useDeepCompareMemo({ xScale, yScale, x_axis_label, threshold, groupedMeasurements});
+
+  // Draw SVG from scratch
+  useEffect(() => {
+    clearMeasurementsSVG(d3Ref.current);
+    drawMeasurementsSVG(d3Ref.current, svgData);
+  }, [svgData]);
+
+  const getPanelTitle = () => {
+    return `${title || "Measurements"} (grouped by ${fields.get(groupBy).title})`;
+  };
 
   const getSVGContainerStyle = () => {
     return {
@@ -15,7 +101,7 @@ const Measurements = ({height, width, showLegend}) => {
   };
 
   return (
-    <Card title="Measurements">
+    <Card title={getPanelTitle()}>
       {showLegend &&
         <ErrorBoundary>
           <Legend right width={width}/>
@@ -25,6 +111,7 @@ const Measurements = ({height, width, showLegend}) => {
         <svg
           id="d3MeasurementsSVG"
           width="100%"
+          ref={d3Ref}
         />
       </div>
     </Card>
---
+++ src/components/measurements/measurementsD3.js
@@ -0,0 +1,172 @@
+import { extent, groups } from "d3-array";
+import { axisBottom, axisLeft } from "d3-axis";
+import { scaleLinear } from "d3-scale";
+import { select } from "d3-selection";
+
+/* C O N S T A N T S */
+const layout = {
+  leftPadding: 120,
+  rightPadding: 30,
+  topPadding: 20,
+  bottomPadding: 50,
+  subplotHeight: 100,
+  subplotPadding: 20,
+  circleRadius: 3,
+  thresholdStrokeWidth: 2,
+  thresholdStroke: "#DDD",
+  subplotFill: "#adb1b3",
+  subplotFillOpacity: "0.15"
+};
+
+const classes = {
+  xAxis: "measurementXAxis",
+  yAxis: "measurementYAxis",
+  threshold: "measurementThreshold",
+  subplot: "measurementSubplot",
+  subplotBackground: "measurementSubplotBackground",
+  rawMeasurements: "rawMeasurements"
+};
+
+const getMeasurementDOMId = (measurement) => `meaurement_${measurement.measurementId}`;
+
+/**
+ * Creates the D3 linear scale for the x-axis with the provided measurements'
+ * values as the domain and the panelWidth with hard-coded padding values as
+ * the range. Expected to be shared across all subplots.
+ * @param {number} panelWidth
+ * @param {Array<Object>} measurements
+ * @returns {function}
+ */
+export const createXScale = (panelWidth, measurements) => {
+  return (
+    scaleLinear()
+      .domain(extent(measurements, (m) => m.value))
+      .range([layout.leftPadding, panelWidth - layout.rightPadding])
+      .nice()
+  );
+};
+
+/**
+ * Creates the D3 linear scale for the y-axis of each individual subplot with
+ * the provided measurements' jitters as the domain and the hard-coded
+ * subplot height as the range.
+ * @param {Array<Object>} measurements
+ * @returns {function}
+ */
+export const createYScale = (measurements) => {
+  return (
+    scaleLinear()
+      .domain(extent(measurements, (m) => m.measurementJitter))
+      .range([layout.subplotHeight, 0])
+      .nice()
+  );
+};
+
+/**
+ * Uses D3.groups() to aggregate measurements into a nested array of groups
+ * The groups are sorted with the most number of measurements first.
+ * @param {Array<Object>} measurements
+ * @param {string} groupBy
+ * @returns {Array<Array<string, Array>>}
+ */
+export const groupMeasurements = (measurements, groupBy) => {
+  const groupedMeasurements = groups(measurements, (d) => d[groupBy]);
+  /*
+   * Using a[1] and b[1] since d3.groups returns a nested array where the
+   * first element is the groupBy field value and the second element is the
+   * array of measurements
+   */
+  return groupedMeasurements.sort((a, b) => b[1].length - a[1].length);
+};
+
+export const clearMeasurementsSVG = (ref) => {
+  select(ref)
+    .attr("height", null)
+    .selectAll("*").remove();
+};
+
+export const drawMeasurementsSVG = (ref, svgData) => {
+  const {xScale, yScale, x_axis_label, threshold, groupedMeasurements} = svgData;
+
+  // Do not draw SVG if there are no measurements
+  if (groupedMeasurements && groupedMeasurements.length === 0) return;
+
+  const svg = select(ref);
+  const svgWidth = svg.node().getBoundingClientRect().width;
+
+  // The number of groups is the number of subplots, which determines the final SVG height
+  const totalSubplotHeight = (layout.subplotHeight * groupedMeasurements.length);
+  const svgHeight = totalSubplotHeight + layout.topPadding + layout.bottomPadding;
+  svg.attr("height", svgHeight);
+
+  // Add threshold if provided
+  if (threshold !== null) {
+    const thresholdXValue = xScale(threshold);
+    svg.append("line")
+      .attr("class", classes.threshold)
+      .attr("x1", thresholdXValue)
+      .attr("x2", thresholdXValue)
+      .attr("y1", layout.topPadding)
+      .attr("y2", svgHeight - layout.bottomPadding)
+      .attr("stroke-width", layout.thresholdStrokeWidth)
+      .attr("stroke", layout.thresholdStroke);
+  }
+
+  // Add x-axis to the bottom of the SVG
+  // (above the bottomPadding to leave room for the x-axis label)
+  svg.append("g")
+    .attr("class", classes.xAxis)
+    .attr("transform", `translate(0, ${svgHeight - layout.bottomPadding})`)
+    .call(axisBottom(xScale))
+    .append("text")
+      .attr("x", layout.leftPadding + ((svgWidth - layout.leftPadding - layout.rightPadding)) / 2)
+      .attr("y", layout.bottomPadding * 2 / 3)
+      .attr("text-anchor", "middle")
+      .attr("fill", "currentColor")
+      .text(x_axis_label);
+
+  // Create a subplot for each grouping
+  let prevSubplotBottom = layout.topPadding;
+  groupedMeasurements.forEach(([groupingValue, measurements], index) => {
+    // Make each subplot it's own SVG to re-use the same subplot yScale
+    const subplot = svg.append("svg")
+      .attr("class", classes.subplot)
+      .attr("width", "100%")
+      .attr("height", layout.subplotHeight)
+      .attr("y", prevSubplotBottom);
+
+    // Add subplot height to keep track of y position for next subplot
+    prevSubplotBottom += layout.subplotHeight;
+
+    // Add a rect to fill the entire width with a light grey background for every other group
+    subplot.append("rect")
+      .attr("class", classes.subplotBackground)
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("height", "100%")
+      .attr("width", "100%")
+      .attr("fill", index % 2 ? layout.subplotFill : "none")
+      .attr("fill-opacity", layout.subplotFillOpacity);
+
+    // Add y axis with a single tick that displays the grouping value
+    subplot.append("g")
+      .attr("class", classes.yAxis)
+      .attr("transform", `translate(${layout.leftPadding}, 0)`)
+      .call(
+        axisLeft(yScale)
+          .ticks(1)
+          .tickFormat(() => groupingValue));
+
+    // Add circles for each measurement
+    subplot.append("g")
+      .selectAll("dot")
+      .data(measurements)
+      .enter()
+      .append("circle")
+        .attr("class", classes.rawMeasurements)
+        .attr("id", (d) => getMeasurementDOMId(d))
+        .attr("cx", (d) => xScale(d.value))
+        .attr("cy", (d) => yScale(d.measurementJitter))
+        .attr("r", layout.circleRadius);
+  });
+};
