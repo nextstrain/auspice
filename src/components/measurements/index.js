nextstrain/auspice

commit 5ae8f239e40709a763bc7a7deed71d67ed7d5e08

Measurements: fix row order for inactive filters
Only use active filters for the group-by field when determining row
order. This allows the rows to follow default ordering if all of the
filters have been inactivated.

src/components/measurements/index.js
@@ -93,10 +93,12 @@ const treeStrainPropertySelector = (state) => {
  *
  * Visibility is indicated by the numeric visibility value in the provided
  * treeStrainVisibility object for strain.
+ *
+ * Returns the active filters object and the filtered measurements
  * @param {Array<Object>} measurements
  * @param {Object<string,number>} treeStrainVisibility
  * @param {Object<string,Map>} filters
- * @returns
+ * @returns {Object<Object, Array>}
  */
 const filterMeasurements = (measurements, treeStrainVisibility, filters) => {
   // Find active filters to filter measurements
@@ -109,15 +111,18 @@ const filterMeasurements = (measurements, treeStrainVisibility, filters) => {
     });
   });
 
-  return measurements.filter((measurement) => {
-    // First check the strain is visible in the tree
-    if (!isVisible(treeStrainVisibility[measurement.strain])) return false;
-    // Then check that the measurement contains values for all active filters
-    for (const [field, values] of Object.entries(activeFilters)) {
-      if (values.length > 0 && !values.includes(measurement[field])) return false;
-    }
-    return true;
-  });
+  return {
+    activeFilters,
+    filteredMeasurements: measurements.filter((measurement) => {
+      // First check the strain is visible in the tree
+      if (!isVisible(treeStrainVisibility[measurement.strain])) return false;
+      // Then check that the measurement contains values for all active filters
+      for (const [field, values] of Object.entries(activeFilters)) {
+        if (values.length > 0 && !values.includes(measurement[field])) return false;
+      }
+      return true;
+    })
+  };
 };
 
 const MeasurementsPlot = ({height, width, showLegend, setPanelTitle}) => {
@@ -138,8 +143,8 @@ const MeasurementsPlot = ({height, width, showLegend, setPanelTitle}) => {
   const [hoverData, setHoverData] = useState(null);
 
   // Filter and group measurements
-  const filteredMeasurements = filterMeasurements(measurements, treeStrainVisibility, filters);
-  const groupedMeasurements = groupMeasurements(filteredMeasurements, groupBy, filters[groupBy]);
+  const {activeFilters, filteredMeasurements} = filterMeasurements(measurements, treeStrainVisibility, filters);
+  const groupedMeasurements = groupMeasurements(filteredMeasurements, groupBy, activeFilters[groupBy]);
 
   // Memoize D3 scale functions to allow deep comparison to work below for svgData
   const xScale = useMemo(() => createXScale(width, measurements), [width, measurements]);

src/components/measurements/measurementsD3.js
@@ -97,7 +97,8 @@ export const createYScale = () => {
  */
 export const groupMeasurements = (measurements, groupBy, groupByFilters) => {
   const groupedMeasurements = groups(measurements, (d) => d[groupBy]);
-  if (groupByFilters) {
+
+  if (groupByFilters && groupByFilters.length) {
     const sortGroupOrder = [...groupByFilters.keys()];
     return orderBy(
       groupedMeasurements,

commit 3db7a968bcd7edf3ed5c920cd433ea257e143f90

Measurements: fix filter ordering of subplots
The ordering of filtered subplots was broken by 5ae8f23 because the
provided filter order was no longer a Map, but a flat Array of values.

This commit changes the function to only expect a single flat array of
values instead of two arrays. This moves the definition of groupings'
order out of `groupMeasurements` to the Measurements component so that
the determination of the order of subplots is not hidden as a side
effect in the grouping function.

src/components/measurements/index.js
@@ -145,7 +145,13 @@ const MeasurementsPlot = ({height, width, showLegend, setPanelTitle}) => {
   // Filter and group measurements
   const {activeFilters, filteredMeasurements} = filterMeasurements(measurements, treeStrainVisibility, filters);
   const groupingOrderedValues = groupings.get(groupBy).values;
-  const groupedMeasurements = groupMeasurements(filteredMeasurements, groupBy, activeFilters[groupBy], groupingOrderedValues);
+  // Default ordering of rows is the groupings value order from redux state
+  let groupByValueOrder = groupingOrderedValues;
+  // If there are active filters for the current group-by field, ordering is the user's filter order
+  if (activeFilters[groupBy] && activeFilters[groupBy].length) {
+    groupByValueOrder = activeFilters[groupBy];
+  }
+  const groupedMeasurements = groupMeasurements(filteredMeasurements, groupBy, groupByValueOrder);
 
   // Memoize D3 scale functions to allow deep comparison to work below for svgData
   const xScale = useMemo(() => createXScale(width, measurements), [width, measurements]);

src/components/measurements/measurementsD3.js
@@ -86,28 +86,15 @@ export const createYScale = () => {
 
 /**
  * Uses D3.groups() to aggregate measurements into a nested array of groups
- * The groups are sorted by the order of values in the provided groupByValues.
- *
- * If groupByFilters Map is provided, sort the groups by the order of the keys.
+ * The groups are sorted by the order of values in the provided groupByValueOrder.
  * @param {Array<Object>} measurements
  * @param {string} groupBy
- * @param {Map<string,Object>} groupByFilters
  * @param {Array<string>} groupByValueOrder
  * @returns {Array<Array<string, Array>>}
  */
-export const groupMeasurements = (measurements, groupBy, groupByFilters, groupByValueOrder) => {
-  const groupedMeasurements = groups(measurements, (d) => d[groupBy]);
-
-  if (groupByFilters && groupByFilters.length) {
-    const sortGroupOrder = [...groupByFilters.keys()];
-    return orderBy(
-      groupedMeasurements,
-      ([groupingValue]) => sortGroupOrder.indexOf(groupingValue),
-      "asc"
-    );
-  }
+export const groupMeasurements = (measurements, groupBy, groupByValueOrder) => {
   return orderBy(
-    groupedMeasurements,
+    groups(measurements, (d) => d[groupBy]),
     ([groupingValue]) => groupByValueOrder.indexOf(groupingValue),
     "asc");
 };