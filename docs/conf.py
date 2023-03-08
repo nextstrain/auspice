65a82cab81ee089dbb004adc83f009d6b692a6db
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-03-08 15:32:14 -0800

    Remove DEV_DOCS.md symlinking
    The link doesn't work properly since the symlink isn't included in the
    rST tree.
    
    Instead of including the symlink in the rST tree, remove it and
    reference the file directly.

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md',
     'narratives',
-    'contributing',
 ]
 
 
