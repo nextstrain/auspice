98ae4d9c54f94d41c7ca7dcebc8e6bbb2abf2af9
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-04-25 15:37:17 -0700

    Remove unused node-fetch import
    node-fetch is no longer necessary as a direct dependency. Move it to
    devDependencies since it is still used in a test.

--- cli/utils.js
+++ cli/utils.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const chalk = require('chalk');
 const path = require("path");
-const fetch = require('node-fetch'); // eslint-disable-line no-unused-vars
 
 const verbose = (msg) => {
   if (global.AUSPICE_VERBOSE) {
