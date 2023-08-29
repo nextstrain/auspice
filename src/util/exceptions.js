85cb6798890a8b57a0a7e62629890f137ab08690
James Hadfield <hadfield.james@gmail.com>
2023-08-29 15:13:37 +1200

    Improve data fetching error handling
    The previous code would (depending on the sidecar file in question)
    catch fetch errors and print console errors, and then also attempt to
    load the (non-existant) data, resulting in more errors. I believe this
    was due to promise chains such as:
    
    promise.then()
      .catch()
      .then()
      .catch()
    
    and the (mis)understanding that if the first catch clause was hit
    then the entire chain would terminate.
    
    The approach implemented here also lets us differentiate between
    fetching + parsing errors, which is a subtle but nice improvement to the
    warning notifications.
    
    (Added comments are also relevant for understanding intended behaviour.)

--- src/util/exceptions.js
+++ src/util/exceptions.js
@@ -4,6 +4,12 @@ export class NoContentError extends Error {
   }
 }
 
+export class FetchError extends Error {
+  constructor(...params) {
+    super(...params);
+  }
+}
+
 /**
  * Thrown when a download produces an empty Newick tree.
  * Usually caused by users trying to download multiple subtrees that do not
