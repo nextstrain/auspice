0615b36470ae70fe4eae57ee17589ce306c1d01a
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-09-12 11:24:36 -0700

    panel-toggles: Convert to TypeScript
    The hooks API is recommended by Redux docs¹.
    
    ¹ https://react-redux.js.org/api/hooks

--- src/components/controls/panel-toggles.tsx
+++ src/components/controls/panel-toggles.tsx
@@ -1,35 +1,52 @@
 import React from "react";
-import { connect } from "react-redux";
-import { withTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from "react-redux";
+import { useTranslation } from 'react-i18next';
 
 import Toggle from "./toggle";
 import { togglePanelDisplay } from "../../actions/panelDisplay";
 
-@connect((state) => ({
-  panelsAvailable: state.controls.panelsAvailable,
-  panelsToDisplay: state.controls.panelsToDisplay,
-  showTreeToo: state.controls.showTreeToo
-}))
-class PanelToggles extends React.Component {
-  render() {
-    const { t } = this.props;
+// Interface to represent the entire Redux store.
+// Since most of the codebase is not typed yet, add types manually¹ for now.
+// TODO: Move this to src/store.
+// ¹ https://react-redux.js.org/using-react-redux/usage-with-typescript#typing-hooks-manually
+interface RootState {
+  controls: {
+    panelsAvailable: string[]
+    panelsToDisplay: string[]
+    showTreeToo: boolean
 
-    const panels = this.props.panelsAvailable.slice();
-    if (this.props.showTreeToo && panels.indexOf("map") !== -1) {
-      panels.splice(panels.indexOf("map"), 1);
-    }
-    return panels.map((n) => (
+    // This allows arbitrary prop names while TypeScript adoption is incomplete.
+    // TODO: add all other props explicitly and remove this.
+    [propName: string]: any;
+  }
+
+  // This allows arbitrary prop names while TypeScript adoption is incomplete.
+  // TODO: add all other props explicitly and remove this.
+  [propName: string]: any;
+}
+
+export default function PanelToggles() {
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  const panelsAvailable = useSelector((state: RootState) => state.controls.panelsAvailable);
+  const panelsToDisplay = useSelector((state: RootState) => state.controls.panelsToDisplay);
+  const showTreeToo = useSelector((state: RootState) => state.controls.showTreeToo);
+
+  const panels = panelsAvailable.slice();
+  if (showTreeToo && panels.indexOf("map") !== -1) {
+    panels.splice(panels.indexOf("map"), 1);
+  }
+  return <>
+    {panels.map((n) => (
       <Toggle
         key={n}
         display
-        on={this.props.panelsToDisplay.indexOf(n) !== -1}
-        callback={() => this.props.dispatch(togglePanelDisplay(n))}
+        on={panelsToDisplay.indexOf(n) !== -1}
+        callback={() => dispatch(togglePanelDisplay(n))}
         label={t("sidebar:Show " + n)}
-        style={{paddingBottom: "10px"}}
+        style={{ paddingBottom: "10px" }}
       />
-    ));
-  }
+    ))}
+  </>
 }
-
-const WithTranslation = withTranslation()(PanelToggles);
-export default WithTranslation;
