8a69ca2c2890839bc2c8fc8f0148d4f788a4e0dc
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-10-18 15:59:30 -0700

    Add ability to show/hide panel options

--- src/components/controls/panelHeader.tsx
+++ src/components/controls/panelHeader.tsx
@@ -4,6 +4,7 @@ import { togglePanelDisplay } from "../../actions/panelDisplay";
 import { HeaderContainer } from "./styles";
 import Toggle from "./toggle";
 import { AnnotatedTitle, Title, Tooltip } from "./annotatedTitle";
+import { PanelChevron } from "./panelChevron";
 
 /** Panel identifier used internally. */
 export type PanelId = string;
@@ -15,30 +16,56 @@ type Props = {
 
   /** Indicates panel visibility. */
   panelIsVisible: boolean
+
+  /** Indicates whether there are options for the panel. */
+  hasOptions: boolean
+
+  /** Indicates options visibility. */
+  optionsAreVisible: boolean
+
+  /** Update options visibility. */
+  setOptionsAreVisible: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 /**
  * A header used by all panel controls, containing an interactive title.
  */
-export const PanelHeader = ({ panel, title, tooltip, panelIsVisible }: Props) => {
+export const PanelHeader = ({ panel, title, tooltip, panelIsVisible, hasOptions, optionsAreVisible, setOptionsAreVisible }: Props) => {
   const dispatch = useAppDispatch();
 
   function togglePanelVisibility() {
     dispatch(togglePanelDisplay(panel))
   }
 
+  function toggleOptionsVisibility() {
+    setOptionsAreVisible(!optionsAreVisible);
+  }
+
   return (
-    <HeaderContainer>
-      <AnnotatedTitle
-        title={title}
-        tooltip={tooltip} />
-      <Toggle
-        display={true}
-        on={panelIsVisible}
-        callback={togglePanelVisibility}
-        label=""
-        style={{ height: "21px" }} // height from ToggleBackground. FIXME: this should be hardcoded in Toggle
-      />
+    <HeaderContainer {...(hasOptions ? {
+                            onClick: toggleOptionsVisibility,
+                            style: {cursor: "pointer"}
+                          } :
+                          {} )}>
+      <span>
+        {hasOptions &&
+          <PanelChevron
+            show={optionsAreVisible} />}
+        <AnnotatedTitle
+          title={title}
+          tooltip={tooltip} />
+      </span>
+      <span
+        // Don't allow the parent click handler to do anything here.
+        onClick={(event) => event.stopPropagation()}>
+        <Toggle
+          display={true}
+          on={panelIsVisible}
+          callback={togglePanelVisibility}
+          label=""
+          style={{ height: "21px" }} // height from ToggleBackground. FIXME: this should be hardcoded in Toggle
+        />
+      </span>
     </HeaderContainer>
   );
 };
