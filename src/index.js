109290ccfcc3cfb0779092abffabe86a16222e31
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-12-15 14:06:03 -0800

    🚧 client: Register service worker

--- src/index.js
+++ src/index.js
@@ -32,6 +32,16 @@ if (!window.NEXTSTRAIN) {window.NEXTSTRAIN = {};}
 /* google analytics */
 initialiseGoogleAnalyticsIfRequired();
 
+if ('serviceWorker' in navigator) {
+ window.addEventListener('load', () => {
+   navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+     console.log('SW registered: ', registration);
+   }).catch((registrationError) => {
+     console.log('SW registration failed: ', registrationError);
+   });
+ });
+}
+
 i18n
   .use(initReactI18next)
   .init({
