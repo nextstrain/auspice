nextstrain/auspice

commit f7e944dd9eee3f19c34d3611f8de916ad2465924

Lift node-selected modal to redux state
Lifting this from tree state to redux state paves the way to fix
inconsistent behaviour as well as allowing the information to be
displayed in a modal outside of the Tree component in the future.

We no longer update the (tip circle) radius within `clearSelectedNode`
as testing (Firefox v121, Chrome v121) shows the `onTipLeave` function
is enough to achieve the desired radius update.

src/components/tree/infoPanels/click.js
@@ -233,43 +233,45 @@ const Trait = ({node, trait, colorings, isTerminal}) => {
 /**
  * A React component to display information about a tree tip in a modal-overlay style
  * @param  {Object}   props
- * @param  {Object}   props.tip              tip node selected
- * @param  {function} props.goAwayCallback
- * @param  {object}   props.colorings
+ * @param  {Object}   props.selectedNode
+ * @param  {Object[]} props.nodes
+ * @param  {function} props.clearSelectedNode
+ * @param  {Object}   props.colorings
+ * @param  {Object}   props.observedMutations
+ * @param  {function} props.geneSortFn
+ * @param  {function} props.t
  */
-const NodeClickedPanel = ({selectedNode, clearSelectedNode, colorings, observedMutations, geneSortFn, t}) => {
-  if (selectedNode.event!=="click") {return null;}
+const NodeClickedPanel = ({selectedNode, nodes, clearSelectedNode, colorings, observedMutations, geneSortFn, t}) => {
+  if (!selectedNode) return null;
+  const node = nodes[selectedNode.idx];
   const panelStyle = { ...infoPanelStyles.panel};
   panelStyle.maxHeight = "70%";
-  const node = selectedNode.node.n;
-  const isTip = selectedNode.type === "tip";
-  const isTerminal = node.fullTipCount===1;
-
-  const title = isTip ?
+  const isTerminal = !node.hasChildren;
+  const title = isTerminal ?
     node.name :
     isTerminal ?
       `Branch leading to ${node.name}` :
       "Internal branch";
 
   return (
-    <div style={infoPanelStyles.modalContainer} onClick={() => clearSelectedNode(selectedNode)}>
+    <div style={infoPanelStyles.modalContainer} onClick={() => clearSelectedNode(selectedNode, isTerminal)}>
       <div className={"panel"} style={panelStyle} onClick={(e) => stopProp(e)}>
         <StrainName>{title}</StrainName>
         <table>
           <tbody>
-            {!isTip && item(t("Number of terminal tips"), node.fullTipCount)}
-            {isTip && <VaccineInfo node={node} t={t}/>}
+            {!isTerminal && item(t("Number of terminal tips"), node.fullTipCount)}
+            {isTerminal && <VaccineInfo node={node} t={t}/>}
             <SampleDate isTerminal={isTerminal} node={node} t={t}/>
-            {!isTip && item("Node name", node.name)}
-            {isTip && <PublicationInfo node={node} t={t}/>}
+            {!isTerminal && item("Node name", node.name)}
+            {isTerminal && <PublicationInfo node={node} t={t}/>}
             {getTraitsToDisplay(node).map((trait) => (
               <Trait node={node} trait={trait} colorings={colorings} key={trait} isTerminal={isTerminal}/>
             ))}
-            {isTip && <AccessionAndUrl node={node}/>}
+            {isTerminal && <AccessionAndUrl node={node}/>}
             {item("", "")}
           </tbody>
         </table>
-        <MutationTable node={node} geneSortFn={geneSortFn} isTip={isTip} observedMutations={observedMutations}/>
+        <MutationTable node={node} geneSortFn={geneSortFn} isTip={isTerminal} observedMutations={observedMutations}/>
         <p style={infoPanelStyles.comment}>
           {t("Click outside this box to go back to the tree")}
         </p>

src/components/tree/reactD3Interface/callbacks.js
@@ -2,6 +2,7 @@ import { updateVisibleTipsAndBranchThicknesses, applyFilter } from "../../../act
 import { NODE_VISIBLE, strainSymbol } from "../../../util/globals";
 import { getDomId, getParentBeyondPolytomy, getIdxOfInViewRootNode } from "../phyloTree/helpers";
 import { branchStrokeForHover, branchStrokeForLeave } from "../phyloTree/renderers";
+import { SELECT_NODE, DESELECT_NODE } from "../../../actions/types";
 
 /* Callbacks used by the tips / branches when hovered / selected */
 
@@ -24,13 +25,7 @@ export const onTipHover = function onTipHover(d) {
 export const onTipClick = function onTipClick(d) {
   if (d.visibility !== NODE_VISIBLE) return;
   if (this.props.narrativeMode) return;
-  this.setState({
-    selectedNode: {
-      node: d,
-      type: "tip",
-      event: "click"
-    }
-  });
+  this.props.dispatch({type: SELECT_NODE, name: d.n.name, idx: d.n.arrayIdx});
   this.props.dispatch(applyFilter("add", strainSymbol, [d.n.name]));
 };
 
@@ -69,13 +64,8 @@ export const onBranchClick = function onBranchClick(d) {
 
   /* if a branch was clicked while holding the shift key, we instead display a node-clicked modal */
   if (window.event.shiftKey) {
-    this.setState({
-      selectedNode: {
-        node: d,
-        type: "branch",
-        event: "click"
-      }
-    });
+    // no need to dispatch a filter action
+    this.props.dispatch({type: SELECT_NODE, name: d.n.name, idx: d.n.arrayIdx})
     return;
   }
 
@@ -141,15 +131,9 @@ export const onTipLeave = function onTipLeave(d) {
 };
 
 /* clearSelectedNode when clicking to remove the node-selected modal */
-export const clearSelectedNode = function clearSelectedNode(selectedNode) {
-  const phylotree = selectedNode.node.that.params.orientation[0] === 1 ?
-    this.state.tree :
-    this.state.treeToo;
-  phylotree.svg.select("#"+getDomId("tip", selectedNode.node.n.name))
-    .attr("r", (dd) => dd["r"]);
-  this.setState({selectedNode: {}});
-  if (selectedNode.type==="tip") {
-    /* restore the tip visibility! */
-    this.props.dispatch(applyFilter("inactivate", strainSymbol, [selectedNode.node.n.name]));
+export const clearSelectedNode = function clearSelectedNode(selectedNode, isTerminal) {
+  if (isTerminal) {
+    this.props.dispatch(applyFilter("inactivate", strainSymbol, [selectedNode.name]));
   }
+  this.props.dispatch({type: DESELECT_NODE});
 };

src/reducers/controls.ts
@@ -71,6 +71,7 @@ export const getDefaultControlsState = () => {
     explodeAttr: undefined,
     selectedBranchLabel: "none",
     showAllBranchLabels: false,
+    selectedNode: null,
     canRenderBranchLabels: true,
     analysisSlider: false,
     geoResolution: defaults.geoResolution,
@@ -232,6 +233,13 @@ const Controls = (state: ControlsState = getDefaultControlsState(), action): Con
       return Object.assign({}, state, {
         geoResolution: action.data
       });
+
+    case types.SELECT_NODE: {
+      return {...state, selectedNode: {name: action.name, idx: action.idx}};
+    }
+    case types.DESELECT_NODE: {
+      return {...state, selectedNode: null}
+    }
     case types.APPLY_FILTER: {
       // values arrive as array
       const filters = Object.assign({}, state.filters, {});

src/actions/types.js
@@ -61,3 +61,6 @@ export const APPLY_MEASUREMENTS_FILTER = "APPLY_MEASUREMENTS_FILTER";
 export const UPDATE_MEASUREMENTS_ERROR = "UPDATE_MEASUREMENTS_ERROR";
 export const TOGGLE_SHOW_ALL_BRANCH_LABELS = "TOGGLE_SHOW_ALL_BRANCH_LABELS";
 export const TOGGLE_MOBILE_DISPLAY = "TOGGLE_MOBILE_DISPLAY";
+export const SELECT_NODE = "SELECT_NODE";
+export const DESELECT_NODE = "DESELECT_NODE";
+

src/components/tree/index.js
@@ -4,6 +4,7 @@ import UnconnectedTree from "./tree";
 const Tree = connect((state) => ({
   tree: state.tree,
   treeToo: state.treeToo,
+  selectedNode: state.controls.selectedNode,
   dateMinNumeric: state.controls.dateMinNumeric,
   dateMaxNumeric: state.controls.dateMaxNumeric,
   quickdraw: state.controls.quickdraw,

src/components/tree/tree.js
@@ -43,8 +43,11 @@ class Tree extends React.Component {
     };
     /* pressing the escape key should dismiss an info modal (if one exists) */
     this.handlekeydownEvent = (event) => {
-      if (event.key==="Escape" && this.state.selectedNode?.node) {
-        this.clearSelectedNode(this.state.selectedNode);
+      if (event.key==="Escape" && this.props.selectedNode) {
+        this.clearSelectedNode(
+          this.props.selectedNode,
+          !this.props.tree.nodes[this.props.selectedNode.idx].hasChildren
+        );
       }
     };
   }
@@ -208,7 +211,8 @@ class Tree extends React.Component {
         />
         <NodeClickedPanel
           clearSelectedNode={this.clearSelectedNode}
-          selectedNode={this.state.selectedNode}
+          selectedNode={this.props.selectedNode}
+          nodes={this.props.tree.nodes}
           observedMutations={this.props.tree.observedMutations}
           colorings={this.props.colorings}
           geneSortFn={this.state.geneSortFn}

commit 4d70916238355fc84f99219865de39c89868a949

Restore terminal branch click behaviour
Clicking on terminal branches incorrectly behaved as if we had clicked
on the tip (the circle); the intended behaviour is for internal +
terminal branches to summarise the information the same way. This was
noticed in an issue comment
<https://github.com/nextstrain/auspice/issues/1752#issuecomment-1950527846>

The bug was introduced in f7e944dd9eee3f19c34d3611f8de916ad2465924 which
removed the ability to detect if a click occurred on a terminal node
(tip) or terminal branch.

src/components/tree/infoPanels/click.js
@@ -247,7 +247,9 @@ const NodeClickedPanel = ({selectedNode, nodes, clearSelectedNode, colorings, ob
   const panelStyle = { ...infoPanelStyles.panel};
   panelStyle.maxHeight = "70%";
   const isTerminal = !node.hasChildren;
-  const title = isTerminal ?
+  const isTip = !selectedNode.isBranch;
+
+  const title = isTip ?
     node.name :
     isTerminal ?
       `Branch leading to ${node.name}` :
@@ -259,19 +261,19 @@ const NodeClickedPanel = ({selectedNode, nodes, clearSelectedNode, colorings, ob
         <StrainName>{title}</StrainName>
         <table>
           <tbody>
-            {!isTerminal && item(t("Number of terminal tips"), node.fullTipCount)}
-            {isTerminal && <VaccineInfo node={node} t={t}/>}
+            {!isTip && item(t("Number of terminal tips"), node.fullTipCount)}
+            {isTip && <VaccineInfo node={node} t={t}/>}
             <SampleDate isTerminal={isTerminal} node={node} t={t}/>
-            {!isTerminal && item("Node name", node.name)}
-            {isTerminal && <PublicationInfo node={node} t={t}/>}
+            {!isTip && item("Node name", node.name)}
+            {isTip && <PublicationInfo node={node} t={t}/>}
             {getTraitsToDisplay(node).map((trait) => (
               <Trait node={node} trait={trait} colorings={colorings} key={trait} isTerminal={isTerminal}/>
             ))}
-            {isTerminal && <AccessionAndUrl node={node}/>}
+            {isTip && <AccessionAndUrl node={node}/>}
             {item("", "")}
           </tbody>
         </table>
-        <MutationTable node={node} geneSortFn={geneSortFn} isTip={isTerminal} observedMutations={observedMutations}/>
+        <MutationTable node={node} geneSortFn={geneSortFn} isTip={isTip} observedMutations={observedMutations}/>
         <p style={infoPanelStyles.comment}>
           {t("Click outside this box to go back to the tree")}
         </p>

src/components/tree/reactD3Interface/callbacks.js
@@ -24,7 +24,7 @@ export const onTipClick = function onTipClick(d) {
   /* The order of these two dispatches is important: the reducer handling
   `SELECT_NODE` must have access to the filtering state _prior_ to these filters
   being applied */
-  this.props.dispatch({type: SELECT_NODE, name: d.n.name, idx: d.n.arrayIdx});
+  this.props.dispatch({type: SELECT_NODE, name: d.n.name, idx: d.n.arrayIdx, isBranch: false});
   this.props.dispatch(applyFilter("add", strainSymbol, [d.n.name]));
 };
 
@@ -60,7 +60,7 @@ export const onBranchClick = function onBranchClick(d) {
   /* if a branch was clicked while holding the shift key, we instead display a node-clicked modal */
   if (window.event.shiftKey) {
     // no need to dispatch a filter action
-    this.props.dispatch({type: SELECT_NODE, name: d.n.name, idx: d.n.arrayIdx})
+    this.props.dispatch({type: SELECT_NODE, name: d.n.name, idx: d.n.arrayIdx, isBranch: true})
     return;
   }
 

src/reducers/controls.ts
@@ -238,7 +238,7 @@ const Controls = (state: ControlsState = getDefaultControlsState(), action): Con
       const existingFilterInfo = (state.filters?.[strainSymbol]||[]).find((info) => info.value===action.name);
       const existingFilterState = existingFilterInfo === undefined ? null :
         existingFilterInfo.active ? 'active' : 'inactive';
-      return {...state, selectedNode: {name: action.name, idx: action.idx, existingFilterState}};
+      return {...state, selectedNode: {name: action.name, idx: action.idx, existingFilterState, isBranch: action.isBranch}};
     }
     case types.DESELECT_NODE: {
       return {...state, selectedNode: null}