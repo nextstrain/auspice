200b851590f5532f0391e0b76289f5da5b38e431
james hadfield <hadfield.james@gmail.com>
2024-10-24 16:41:33 +1300

    WIP fixup

--- src/actions/streamTrees.js
+++ src/actions/streamTrees.js
@@ -6,7 +6,7 @@ export function toggleStreamTree() {
   return function(dispatch, getState) {
     const {controls, tree} = getState();
     const showStreamTrees = !controls.showStreamTrees;
-    const streams = partitionIntoStreams(showStreamTrees, tree.nodes, controls.colorScale, controls.absoluteDateMinNumeric, controls.absoluteDateMaxNumeric)
+    const streams = partitionIntoStreams(showStreamTrees, tree.nodes, tree.visibility, controls.colorScale, controls.absoluteDateMinNumeric, controls.absoluteDateMaxNumeric)
     console.log("THUNK::New streams structure:", streams)
     dispatch({type: TOGGLE_STREAM_TREE, showStreamTrees, streams})
   }
