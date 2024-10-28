0c91724786d5b7f556defbcd002a9c003fac4962
james hadfield <hadfield.james@gmail.com>
2024-10-28 13:52:20 +1300

    [entropy] improve performance of entropy calcs
    by not recalculating data on the leading edge of the debounced
    recalculation action. For big trees (which take >500ms of time to
    redraw) the main thread is still blocked for roughly the same amount of
    time, but the tree is redrawn faster. For small trees which redraw
    quicker than that the entropy doesn't update until the debounce 500ms
    timeout is reached, resulting in slightly odd behaviour.
    
    (Reducing this timout also results in less-than-ideal behaviour as
    (e.g.) dragging the date range of a tree results in interruptions while
    the entropy calcs run which is worse IMO.)

--- src/actions/entropy.js
+++ src/actions/entropy.js
@@ -15,7 +15,7 @@ export const updateEntropyVisibility = debounce((dispatch, getState) => {
   ) {return;}
   const [data, maxYVal] = calcEntropyInView(tree.nodes, tree.visibility, entropy.selectedCds, entropy.showCounts);
   dispatch({type: types.ENTROPY_DATA, data, maxYVal});
-}, 500, { leading: true, trailing: true });
+}, 500, { leading: false, trailing: true });
 
 /**
  * Returns a thunk which makes zero or one dispatches to update the entropy reducer
