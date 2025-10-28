f36ad0676d2d34e8e28d3e2bfc218c522b231943
james hadfield <jameshadfield@users.noreply.github.com>
2025-10-28 18:34:11 +1300

    [info modal] shift-click multiple mutations
    To add to / remove-from the color-by.

--- test/genotype.test.js
+++ test/genotype.test.js
@@ -1,4 +1,4 @@
-import { encodeGenotypeFilters, decodeGenotypeFilters } from "../src/util/getGenotype";
+import { encodeGenotypeFilters, decodeGenotypeFilters, encodeColorByGenotypeCumulative } from "../src/util/getGenotype";
 import { sortConstellationLongFn } from "../src/util/treeVisibilityHelpers";
 
 const filtersToQuery = [
@@ -45,4 +45,49 @@ test("Genotype sorting function", () => {
   constellationsToSort.forEach(([unsorted, sorted]) => {
     expect(unsorted.sort(sortConstellationLongFn)).toStrictEqual(sorted);
   });
+});
+
+/* -----------------  encodeColorByGenotypeCumulative  ----------------- */
+test("encodeColorByGenotypeCumulative replaces a non-genotype color-by", () => {
+  expect(
+    encodeColorByGenotypeCumulative({currentColorBy: 'foo', gene: 'NS3', position: 584})
+  ).toStrictEqual("gt-NS3_584");
+});
+test("encodeColorByGenotypeCumulative handles positions supplied as strings", () => {
+  expect(
+    encodeColorByGenotypeCumulative({currentColorBy: 'foo', gene: 'NS3', position: 584})
+  ).toStrictEqual("gt-NS3_584");
+});
+
+test("encodeColorByGenotypeCumulative returns the existing color-by for a malformed position argument", () => {
+  expect(
+    encodeColorByGenotypeCumulative({currentColorBy: 'foo', gene: 'NS3', position: 'bad!'})
+  ).toStrictEqual("foo");
+});
+
+test("encodeColorByGenotypeCumulative replaces a genotype color-by of a different gene", () => {
+  expect(
+    encodeColorByGenotypeCumulative({currentColorBy: 'gt-NS3_584', gene: 'nuc', position: 123})
+  ).toStrictEqual("gt-nuc_123");
+  expect(
+    encodeColorByGenotypeCumulative({currentColorBy: 'gt-nuc_123', gene: 'NS3', position: 584})
+  ).toStrictEqual("gt-NS3_584");
+});
+
+test("encodeColorByGenotypeCumulative adds positions (and orders them)", () => {
+  expect(
+    encodeColorByGenotypeCumulative({currentColorBy: 'gt-NS3_584', gene: 'NS3', position: 123})
+  ).toStrictEqual("gt-NS3_123,584");
+});
+
+test("encodeColorByGenotypeCumulative removes existing positions", () => {
+  expect(
+    encodeColorByGenotypeCumulative({currentColorBy: 'gt-NS3_123,584', gene: 'NS3', position: 123})
+  ).toStrictEqual("gt-NS3_584");
+});
+
+test("encodeColorByGenotypeCumulative doesn't remove a single existing position", () => {
+  expect(
+    encodeColorByGenotypeCumulative({currentColorBy: 'gt-NS3_123', gene: 'NS3', position: 123})
+  ).toStrictEqual("gt-NS3_123");
 });
