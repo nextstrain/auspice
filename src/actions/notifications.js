bcc253efa414c43645fd45522769517a82477cf6
Jover Lee <joverlee521@gmail.com>
2026-05-01 13:07:42 -0700

    Support notifications that do not automatically close
    Previously, only error notifications would be static and not
    automatically close. I want to add a warning notification that
    is static for the dataset editor so I'm making it possible to explicitly
    set whether a notification will automatically close. The default values
    should replicate the previous behavior, so existing notifications do
    not need to be updated.

--- src/actions/notifications.js
+++ src/actions/notifications.js
@@ -9,7 +9,7 @@ const triggerNotification = (data) => {
       type: types.ADD_NOTIFICATION,
       id
     }));
-    if (data.notificationType !== "error") {
+    if (data.autoClose) {
       window.setTimeout(() => dispatch({
         type: types.REMOVE_NOTIFICATION,
         id
@@ -18,15 +18,15 @@ const triggerNotification = (data) => {
   };
 };
 
-export const infoNotification = ({message = "Info", details = ""} = {}) => {
-  return triggerNotification({notificationType: "info", message, details});
+export const infoNotification = ({message = "Info", details = "", autoClose = true} = {}) => {
+  return triggerNotification({notificationType: "info", message, details, autoClose});
 };
-export const errorNotification = ({message = "Error", details = ""} = {}) => {
-  return triggerNotification({notificationType: "error", message, details});
+export const errorNotification = ({message = "Error", details = "", autoClose = false} = {}) => {
+  return triggerNotification({notificationType: "error", message, details, autoClose});
 };
-export const successNotification = ({message = "Success!", details = ""} = {}) => {
-  return triggerNotification({notificationType: "success", message, details});
+export const successNotification = ({message = "Success!", details = "", autoClose = true} = {}) => {
+  return triggerNotification({notificationType: "success", message, details, autoClose});
 };
-export const warningNotification = ({message = "Warning", details = ""} = {}) => {
-  return triggerNotification({notificationType: "warning", message, details});
+export const warningNotification = ({message = "Warning", details = "", autoClose = true} = {}) => {
+  return triggerNotification({notificationType: "warning", message, details, autoClose});
 };
